d61108f504fbe9d32a379626f439261f74dd8688
Rex Aliis <rexaliisdev@gmail.com>
2025-01-04 18:10:15 -0300

    add modifyowncharacter endpoint

---
+++ WhiteTale.Server/Features/Characters/ModifyOwnCharacter.cs
@@ -0,0 +1,73 @@
+﻿using System.Security.Claims;
+using WhiteTale.Server.Common.RateLimiting;
+using WhiteTale.Server.Domain.Users;
+
+namespace WhiteTale.Server.Features.Characters;
+
+internal sealed class ModifyOwnCharacter : IEndpoint
+{
+	public void Build(IEndpointRouteBuilder builder)
+	{
+		_ = builder.MapPatch("api/characters/@me", HandleAsync)
+			.RequireRateLimiting(RateLimitPolicyNames.Global)
+			.RequireAuthorization(AuthorizationPolicyNames.BearerToken);
+	}
+
+	private static async Task<Results<Ok<CharacterData>, ProblemHttpResult, InternalServerError>> HandleAsync(
+		[FromBody] ModifyOwnCharacterRequestBody body,
+		[FromServices] HttpContext httpContext,
+		[FromServices] UserManager<User> userManager,
+		[FromServices] IValidator<ModifyOwnCharacterRequestBody> bodyValidator,
+		[FromServices] ApplicationDbContext dbContext,
+		[FromServices] ILogger logger)
+	{
+		var bodyValidation = await bodyValidator.ValidateAsync(body).ConfigureAwait(false);
+		if (!bodyValidation.IsValid)
+		{
+			var problemDetails = bodyValidation.Errors.ToProblemDetails();
+			return TypedResults.Problem(problemDetails);
+		}
+
+		var userIdClaimValue = userManager.GetUserId(httpContext.User);
+		if (!UInt64.TryParse(userIdClaimValue, out var userId))
+		{
+			return TypedResults.InternalServerError();
+		}
+
+		var character = await dbContext.Characters
+			.AsTracking()
+			.Where(character => character.Id == userId)
+			.SingleOrDefaultAsync()
+			.ConfigureAwait(false);
+		if (character is null)
+		{
+			return TypedResults.InternalServerError();
+		}
+
+		_ = dbContext.Attach(character);
+		character.DisplayName = body.DisplayName ?? character.DisplayName;
+		character.Description = body.Description ?? character.Description;
+		character.ConcurrencyStamp = Guid.NewGuid().ToString();
+
+		try
+		{
+			_ = await dbContext.SaveChangesAsync().ConfigureAwait(false);
+		}
+		catch (DbUpdateConcurrencyException ex)
+		{
+			logger.LogDbUpdateConcurrencyProblem(nameof(User), character.Id, ex);
+			return TypedResults.InternalServerError();
+		}
+
+		return TypedResults.Ok(new CharacterData
+		{
+			Id = character.Id,
+			DisplayName = character.DisplayName,
+			Description = character.Description,
+			CurrentRoomId = character.CurrentRoomId,
+			Presence = character.Presence,
+			OwnerType = character.OwnerType,
+			Permissions = character.Permissions
+		});
+	}
+}
---
+++ WhiteTale.Server/Features/Characters/ModifyOwnCharacterRequestBody.cs
@@ -0,0 +1,26 @@
+﻿namespace WhiteTale.Server.Features.Characters;
+
+internal sealed class ModifyOwnCharacterRequestBody
+{
+	private readonly String? _description;
+
+	private readonly String? _displayName;
+
+	/// <summary>
+	///     The name of the character.
+	/// </summary>
+	public String? DisplayName
+	{
+		get => _displayName;
+		init => _displayName = value?.Trim();
+	}
+
+	/// <summary>
+	///     The description of the character.
+	/// </summary>
+	public String? Description
+	{
+		get => _description;
+		init => _description = value?.Trim();
+	}
+}
