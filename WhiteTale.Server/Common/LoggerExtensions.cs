a61d2c850408bc858e2c5cfd944804ba5d562e1a
Rex Aliis <rexaliisdev@gmail.com>
2025-01-04 16:08:25 -0300

    move logging to Common namespace, add new methods
    rename LogMessage to Inform, add trace and a db related log method.

---
+++ WhiteTale.Server/Common/LoggerExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace WhiteTale.Server.Common;
+
+internal static partial class LoggerExtensions
+{
+	[LoggerMessage(LogLevel.Information, "{message}")]
+	internal static partial void Inform(this ILogger logger, String message);
+
+	[LoggerMessage(LogLevel.Trace, "{message}")]
+	internal static partial void Trace(this ILogger logger, String message);
+
+	[LoggerMessage(LogLevel.Error, "{tableName} with id '{id}' has been modified since it was loaded into memory.")]
+	internal static partial void LogDbUpdateConcurrencyProblem(this ILogger logger, String tableName, Object id, Exception ex);
+}
--- WhiteTale.Server/Common/Logging/LoggerExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Extensions.Logging;
-
-namespace WhiteTale.Server.Common.Logging;
-
-internal static partial class LoggerExtensions
-{
-	[LoggerMessage(LogLevel.Information, "{message}")]
-	public static partial void LogMessage(this ILogger logger, string message);
-}
