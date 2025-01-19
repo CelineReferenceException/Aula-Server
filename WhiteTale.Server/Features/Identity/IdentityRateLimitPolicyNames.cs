93b9e9a91564a337aaf92043c0fe957e523d9f7b
Rex Aliis <rexaliisdev@gmail.com>
2025-01-19 01:49:02 -0300

    remove ForgotPassword specific ratelimit policy

--- WhiteTale.Server/Features/Identity/IdentityRateLimitPolicyNames.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace WhiteTale.Server.Features.Identity;
-
-internal static class IdentityRateLimitPolicyNames
-{
-	internal const String ForgotPassword = $"{nameof(IdentityRateLimitPolicyNames)}:{nameof(ForgotPassword)}";
-}
