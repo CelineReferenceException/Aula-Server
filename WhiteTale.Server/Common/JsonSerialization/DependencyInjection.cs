c9881051197b7b942189fec806ebc762d1d0d2f9
Rex Aliis <rexaliisdev@gmail.com>
2025-01-20 01:17:06 -0300

    add json converters for 64bits numbers

---
+++ WhiteTale.Server/Common/JsonSerialization/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using System.Text.Json.Serialization;
+using JsonOptions = Microsoft.AspNetCore.Http.Json.JsonOptions;
+
+namespace WhiteTale.Server.Common.JsonSerialization;
+
+internal static class DependencyInjection
+{
+	internal static IServiceCollection AddJsonSerialization(this IServiceCollection services)
+	{
+		_ = services.Configure<JsonOptions>(options =>
+		{
+			var converters = typeof(IAssemblyMarker).Assembly.DefinedTypes
+				.Where(x => x.BaseType is not null && !x.IsGenericType && x.IsAssignableTo(typeof(JsonConverter)));
+
+			foreach (var converter in converters)
+			{
+				var instance = Activator.CreateInstance(converter) as JsonConverter ?? throw new UnreachableException();
+				options.SerializerOptions.Converters.Add(instance);
+			}
+		});
+
+		return services;
+	}
+}
---
+++ WhiteTale.Server/Common/JsonSerialization/Int64StringConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace WhiteTale.Server.Common.JsonSerialization;
+
+internal sealed class Int64StringConverter : JsonConverter<Int64>
+{
+	[SuppressMessage("Style", "IDE0072:Add missing cases")]
+	public override Int64 Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+	{
+		return reader.TokenType switch
+		{
+			JsonTokenType.Number => reader.GetInt64(),
+			JsonTokenType.String => Int64.Parse(reader.ValueSpan, CultureInfo.InvariantCulture),
+			_ => throw new JsonException()
+		};
+	}
+
+	public override void Write(Utf8JsonWriter writer, Int64 value, JsonSerializerOptions options)
+	{
+		writer.WriteStringValue(value.ToString(CultureInfo.InvariantCulture));
+	}
+}
