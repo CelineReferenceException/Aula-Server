4400e8e2576f1447ab9597b37ec12c2fd6389c64
Rex Aliis <rexaliisdev@gmail.com>
2025-01-21 20:09:09 -0300

    add api endpoint for removing all room connections at once

---
+++ WhiteTale.Server/Features/Rooms/Connections/RemoveAllConnections.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+
+namespace WhiteTale.Server.Features.Rooms.Connections;
+
+internal sealed class RemoveAllConnections : IEndpoint
+{
+	public void Build(IEndpointRouteBuilder route)
+	{
+		_ = route.MapDelete("api/rooms/{sourceRoomId}/connections", HandleAsync)
+			.RequireRateLimiting(CommonRateLimitPolicyNames.Global)
+			.RequireAuthorization(IdentityAuthorizationPolicyNames.BearerToken)
+			.RequirePermission(Permissions.ManageRooms);
+	}
+
+	private static async Task<Results<Ok<IEnumerable<UInt64>>, ProblemHttpResult>> HandleAsync(
+		[FromRoute] UInt64 sourceRoomId,
+		[FromServices] ApplicationDbContext dbContext,
+		[FromServices] SnowflakeGenerator snowflakeGenerator)
+	{
+		var sourceRoomExists = await dbContext.Rooms
+			.AsNoTracking()
+			.AnyAsync(room => room.Id == sourceRoomId);
+		if (!sourceRoomExists)
+		{
+			return TypedResults.Problem(new ProblemDetails
+			{
+				Title = "Invalid room",
+				Detail = "The room does not exist.",
+			});
+		}
+
+		var connections = await dbContext.RoomConnections
+			.AsTracking()
+			.Where(connection => connection.SourceRoomId == sourceRoomId)
+			.ToListAsync();
+
+		connections.ForEach(connection => connection.Remove());
+		dbContext.RoomConnections.RemoveRange(connections);
+		_ = await dbContext.SaveChangesAsync();
+
+		return TypedResults.Ok(connections.Select(connection => connection.TargetRoomId));
+	}
+}
