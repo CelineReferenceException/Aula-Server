ad30c2a3ba84d452c3fe6da9102392380d789a86
Rex Aliis <rexaliisdev@gmail.com>
2025-01-23 18:18:35 -0300

    add presence options

---
+++ WhiteTale.Server/Features/Gateway/Events/Receive/Presences/PresenceOptions.cs
@@ -0,0 +1,17 @@
+﻿namespace WhiteTale.Server.Features.Gateway.Events.Receive.Presences;
+
+/// <summary>
+///     Define available presences statuses to select for a user.
+/// </summary>
+internal enum PresenceOptions
+{
+	/// <summary>
+	///     Show the user as offline.
+	/// </summary>
+	Invisible = 0,
+
+	/// <summary>
+	///     Show the user as online.
+	/// </summary>
+	Online = 1,
+}
