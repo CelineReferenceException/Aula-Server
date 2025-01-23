0dc321b1248c3b1bf6f0bce2d7c355e7bb9b5681
Rex Aliis <rexaliisdev@gmail.com>
2025-01-23 18:33:11 -0300

    add custom ratelimit for gateway connections

--- WhiteTale.Server/Features/Gateway/DependencyInjection.cs
+++ WhiteTale.Server/Features/Gateway/DependencyInjection.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.WebSockets;
+﻿using System.Threading.RateLimiting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.WebSockets;
+using Microsoft.Extensions.Options;
 
 namespace WhiteTale.Server.Features.Gateway;
 
@@ -11,6 +16,44 @@ internal static class DependencyInjection
 		{
 			options.KeepAliveInterval = TimeSpan.Zero;
 			options.KeepAliveTimeout = TimeSpan.FromSeconds(60);
+		});
+
+		_ = services.AddOptions<RateLimitOptions>(GatewayRateLimitPolicyNames.Default)
+			.BindConfiguration($"RateLimiters:{nameof(GatewayRateLimitPolicyNames.Default)}")
+			.ValidateDataAnnotations()
+			.ValidateOnStart();
+
+		_ = services.PostConfigure<RateLimitOptions>(GatewayRateLimitPolicyNames.Default, options =>
+		{
+			options.WindowMilliseconds ??= (Int32)TimeSpan.FromHours(24).TotalMilliseconds;
+			options.PermitLimit ??= 1000;
+		});
+
+		_ = services.AddRateLimiter(options =>
+		{
+			options.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+
+			_ = options.AddPolicy(GatewayRateLimitPolicyNames.Default, httpContext =>
+			{
+				var userManager = ServiceProviderServiceExtensions.GetRequiredService<UserManager<User>>(httpContext.RequestServices);
+
+				var userId = userManager.GetUserId(httpContext.User);
+				var partitionKey = userId ?? httpContext.Connection.RemoteIpAddress?.ToString() ?? String.Empty;
+
+				var rateLimit = httpContext.RequestServices
+					.GetRequiredService<IOptionsSnapshot<RateLimitOptions>>()
+					.Get(GatewayRateLimitPolicyNames.Default);
+				var permitLimit = rateLimit.PermitLimit!.Value;
+				var window = TimeSpan.FromMilliseconds(rateLimit.WindowMilliseconds!.Value);
+
+				return RateLimitPartition.GetFixedWindowLimiter(partitionKey,
+					_ => new FixedWindowRateLimiterOptions
+					{
+						PermitLimit = permitLimit,
+						Window = window,
+						AutoReplenishment = true,
+					});
+			});
 		});
 
 		return services;
---
+++ WhiteTale.Server/Features/Gateway/GatewayRateLimitPolicyNames.cs
@@ -0,0 +1,8 @@
+﻿namespace WhiteTale.Server.Features.Gateway;
+
+internal static class GatewayRateLimitPolicyNames
+{
+	private const String Prefix = nameof(GatewayRateLimitPolicyNames);
+
+	internal const String Default = $"{Prefix}.{nameof(Default)}";
+}
