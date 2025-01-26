f31707f28dff1970ad8b52bf9440b0483f8fbfa7
Rex Aliis <rexaliisdev@gmail.com>
2025-01-26 11:27:44 -0300

    add CommandLine to global usings

--- WhiteTale.Server/Common/GlobalUsings.cs
+++ WhiteTale.Server/Common/GlobalUsings.cs
@@ -6,6 +6,7 @@ global using System.Threading.Tasks;
 global using WhiteTale.Server.Common;
 global using WhiteTale.Server.Common.AuthorizationRequirements;
 global using WhiteTale.Server.Common.BackgroundTaskQueue;
+global using WhiteTale.Server.Common.CommandLine;
 global using WhiteTale.Server.Common.Endpoints;
 global using WhiteTale.Server.Common.Identity;
 global using WhiteTale.Server.Common.Logging;
