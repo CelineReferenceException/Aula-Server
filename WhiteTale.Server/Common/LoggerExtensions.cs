c8a05388b9b5dd409a3535ca0fd39b5516945932
Rex Aliis <rexaliisdev@gmail.com>
2025-01-29 01:10:36 -0300

    remove unused log method

--- WhiteTale.Server/Common/LoggerExtensions.cs
+++ WhiteTale.Server/Common/LoggerExtensions.cs
@@ -7,7 +7,4 @@ internal static partial class LoggerExtensions
 
 	[LoggerMessage(LogLevel.Trace, "{message}")]
 	internal static partial void Trace(this ILogger logger, String message);
-
-	[LoggerMessage(LogLevel.Error, "{tableName} with id '{id}' has been modified since it was loaded into memory.")]
-	internal static partial void LogDbUpdateConcurrencyProblem(this ILogger logger, String tableName, Object id, Exception ex);
 }
