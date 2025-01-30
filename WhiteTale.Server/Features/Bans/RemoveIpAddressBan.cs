72bb768d8f23a6889d6ef47507f1e07ab9851737
Rex Aliis <rexaliisdev@gmail.com>
2025-01-30 08:47:30 -0300

    add the remove ban endpoints

---
+++ WhiteTale.Server/Features/Bans/RemoveIpAddressBan.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+
+namespace WhiteTale.Server.Features.Bans;
+
+internal sealed class RemoveIpAddressBan : IEndpoint
+{
+	public void Build(IEndpointRouteBuilder route)
+	{
+		_ = route.MapDelete("bans/ip-address/{ipAddress}", HandleAsync)
+			.RequireRateLimiting(CommonRateLimitPolicyNames.Global)
+			.RequireAuthorization(IdentityAuthorizationPolicyNames.BearerToken)
+			.RequirePermissions(Permissions.BanUsers)
+			.DenyBannedUsers()
+			.HasApiVersion(1);
+	}
+
+	private static async Task<Results<NoContent, ProblemHttpResult, InternalServerError>> HandleAsync(
+		[FromRoute] String ipAddress,
+		[FromServices] IpAddressValidator ipAddressValidator,
+		[FromServices] ApplicationDbContext dbContext)
+	{
+		var ipValidation = await ipAddressValidator.ValidateAsync(ipAddress);
+		if (!ipValidation.IsValid)
+		{
+			var problemDetails = ipValidation.Errors.ToProblemDetails();
+			return TypedResults.Problem(problemDetails);
+		}
+
+		var ban = await dbContext.Bans
+			.AsTracking()
+			.Where(x => x.IpAddress == ipAddress)
+			.FirstOrDefaultAsync();
+		if (ban is null)
+		{
+			return TypedResults.NoContent();
+		}
+
+		ban.Remove();
+		_ = dbContext.Bans.Remove(ban);
+
+		try
+		{
+			_ = await dbContext.SaveChangesAsync();
+		}
+		catch (DbUpdateConcurrencyException)
+		{
+			return TypedResults.InternalServerError();
+		}
+
+		return TypedResults.NoContent();
+	}
+}
