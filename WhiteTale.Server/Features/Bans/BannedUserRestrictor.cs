971e2741f950ba3c41d66e4e5be9d095e5153d63
Rex Aliis <rexaliisdev@gmail.com>
2025-01-31 03:36:17 -0300

    add retry policy if banned user update fails

--- WhiteTale.Server/Features/Bans/BannedUserRestrictor.cs
+++ WhiteTale.Server/Features/Bans/BannedUserRestrictor.cs
@@ -6,10 +6,12 @@ namespace WhiteTale.Server.Features.Bans;
 internal sealed class BannedUserRestrictor : INotificationHandler<BanCreatedEvent>
 {
 	private readonly ApplicationDbContext _dbContext;
+	private readonly ResiliencePipelines _resiliencePipelines;
 
-	public BannedUserRestrictor(ApplicationDbContext dbContext)
+	public BannedUserRestrictor(ApplicationDbContext dbContext, ResiliencePipelines resiliencePipelines)
 	{
 		_dbContext = dbContext;
+		_resiliencePipelines = resiliencePipelines;
 	}
 
 	public async Task Handle(BanCreatedEvent notification, CancellationToken cancellationToken)
@@ -20,18 +22,21 @@ internal sealed class BannedUserRestrictor : INotificationHandler<BanCreatedEven
 			return;
 		}
 
-		var user = await _dbContext.Users
-			.AsTracking()
-			.Where(u => u.Id == ban.TargetId)
-			.FirstOrDefaultAsync(cancellationToken);
-		if (user is null)
+		await _resiliencePipelines.RetryOnDbConcurrencyProblem.ExecuteAsync(async ct =>
 		{
-			return;
-		}
+			var user = await _dbContext.Users
+				.AsTracking()
+				.Where(u => u.Id == ban.TargetId)
+				.FirstOrDefaultAsync(ct);
+			if (user is null)
+			{
+				return;
+			}
 
-		user.Modify(permissions: 0);
-		user.UpdateConcurrencyStamp();
+			user.Modify(permissions: 0);
+			user.UpdateConcurrencyStamp();
 
-		_ = await _dbContext.SaveChangesAsync(cancellationToken);
+			_ = await _dbContext.SaveChangesAsync(ct);
+		}, cancellationToken);
 	}
 }
