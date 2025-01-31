ef0618c0f58de8105011921ee76f7d73bea7bcc6
Rex Aliis <rexaliisdev@gmail.com>
2025-01-31 08:08:41 -0300

    add ban gateway events

---
+++ WhiteTale.Server/Features/Gateway/Events/Bans/BanRemovedEventHandler.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.Json;
+using MediatR;
+using Microsoft.AspNetCore.Http.Json;
+using Microsoft.Extensions.Options;
+using WhiteTale.Server.Features.Bans;
+
+namespace WhiteTale.Server.Features.Gateway.Events.Bans;
+
+internal sealed class BanRemovedEventHandler : INotificationHandler<BanRemovedEvent>
+{
+	private readonly JsonSerializerOptions _jsonSerializerOptions;
+
+	public BanRemovedEventHandler(IOptions<JsonOptions> jsonOptions)
+	{
+		_jsonSerializerOptions = jsonOptions.Value.SerializerOptions;
+	}
+
+	public Task Handle(BanRemovedEvent notification, CancellationToken cancellationToken)
+	{
+		var ban = notification.Ban;
+		var payload = new GatewayPayload<BanData>
+		{
+			Operation = OperationType.Dispatch,
+			Event = EventType.BanRemoved,
+			Data = new BanData
+			{
+				Type = ban.Type,
+				ExecutorId = ban.ExecutorId,
+				Reason = ban.Reason,
+				TargetId = ban.TargetId,
+				IpAddress = ban.IpAddress,
+				CreationTime = ban.CreationTime,
+			},
+		}.GetJsonUtf8Bytes(_jsonSerializerOptions);
+
+		foreach (var session in ConnectToGateway.Sessions.Values)
+		{
+			if (!session.Intents.HasFlag(Intents.Moderation))
+			{
+				continue;
+			}
+
+			_ = session.QueueEventAsync(payload, cancellationToken);
+		}
+
+		return Task.CompletedTask;
+	}
+}
