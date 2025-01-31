bd38a02ca154f70fe48990f7da23c85e80a12566
Rex Aliis <rexaliisdev@gmail.com>
2025-01-31 10:36:56 -0300

    remove innecessary db query by using cached user

--- WhiteTale.Server/Features/Users/GetOwnUser.cs
+++ WhiteTale.Server/Features/Users/GetOwnUser.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
-using Microsoft.EntityFrameworkCore;
 
 namespace WhiteTale.Server.Features.Users;
 
@@ -21,35 +20,23 @@ internal sealed class GetOwnUser : IEndpoint
 
 	private static async Task<Results<Ok<UserData>, InternalServerError>> HandleAsync(
 		HttpContext httpContext,
-		[FromServices] UserManager<User> userManager,
-		[FromServices] ApplicationDbContext dbContext)
+		[FromServices] UserManager<User> userManager)
 	{
-		var userIdClaimValue = userManager.GetUserId(httpContext.User);
-		if (!UInt64.TryParse(userIdClaimValue, out var userId))
-		{
-			return TypedResults.InternalServerError();
-		}
-
-		var user = await dbContext.Users
-			.AsNoTracking()
-			.Where(u => u.Id == userId)
-			.Select(u =>
-				new UserData
-				{
-					Id = userId,
-					DisplayName = u.DisplayName,
-					Description = u.Description,
-					CurrentRoomId = u.CurrentRoomId,
-					OwnerType = u.OwnerType,
-					Presence = u.Presence,
-					Permissions = u.Permissions,
-				})
-			.FirstOrDefaultAsync();
+		var user = await userManager.GetUserAsync(httpContext.User);
 		if (user is null)
 		{
 			return TypedResults.InternalServerError();
 		}
 
-		return TypedResults.Ok(user);
+		return TypedResults.Ok(new UserData
+		{
+			Id = user.Id,
+			DisplayName = user.DisplayName,
+			Description = user.Description,
+			CurrentRoomId = user.CurrentRoomId,
+			OwnerType = user.OwnerType,
+			Presence = user.Presence,
+			Permissions = user.Permissions,
+		});
 	}
 }
