d8859d2b7833be24493b589f579c3ac4563eaea4
Rex Aliis <rexaliisdev@gmail.com>
2025-01-31 07:25:00 -0300

    add get users bans endpoint

---
+++ WhiteTale.Server/Features/Bans/GetUserBans.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+
+namespace WhiteTale.Server.Features.Bans;
+
+internal sealed class GetUserBans : IEndpoint
+{
+	public void Build(IEndpointRouteBuilder route)
+	{
+		_ = route.MapGet("bans/users", HandleAsync)
+			.RequireRateLimiting(RateLimitPolicyNames.Global)
+			.RequireAuthorization(IdentityAuthorizationPolicyNames.BearerToken)
+			.RequirePermissions(Permissions.BanUsers)
+			.DenyBannedUsers()
+			.HasApiVersion(1);
+	}
+
+	private static async Task<Ok<List<BanData>>> HandleAsync([FromServices] ApplicationDbContext dbContext)
+	{
+		var bans = await dbContext.Bans
+			.Where(b => b.Type == BanType.Id)
+			.Select(b => new BanData
+			{
+				Type = b.Type,
+				ExecutorId = b.ExecutorId,
+				Reason = b.Reason,
+				TargetId = b.TargetId,
+				IpAddress = b.IpAddress,
+				CreationTime = b.CreationTime,
+			})
+			.ToListAsync();
+
+		return TypedResults.Ok(bans);
+	}
+}
