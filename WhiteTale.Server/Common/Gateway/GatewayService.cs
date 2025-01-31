a2aa6e977a90258f521689e8bfc214e590049528
Rex Aliis <rexaliisdev@gmail.com>
2025-01-31 10:13:52 -0300

    create service scope for gateway service

--- WhiteTale.Server/Common/Gateway/GatewayService.cs
+++ WhiteTale.Server/Common/Gateway/GatewayService.cs
@@ -10,16 +10,18 @@ internal sealed class GatewayService
 {
 	private readonly ConcurrentDictionary<String, GatewaySession> _sessions = [];
 	private readonly JsonSerializerOptions _jsonSerializerOptions;
+	private readonly IServiceScope _serviceScope;
 	private readonly IPublisher _publisher;
 
 	internal TimeSpan TimeToExpire { get; } = TimeSpan.FromSeconds(60);
 
 	internal IReadOnlyDictionary<String, GatewaySession> Sessions => _sessions;
 
-	public GatewayService(IOptions<JsonOptions> jsonOptions, IPublisher publisher)
+	public GatewayService(IOptions<JsonOptions> jsonOptions, IServiceProvider serviceProvider)
 	{
 		_jsonSerializerOptions = jsonOptions.Value.SerializerOptions;
-		_publisher = publisher;
+		_serviceScope = serviceProvider.CreateScope();
+		_publisher = _serviceScope.ServiceProvider.GetRequiredService<IPublisher>();
 	}
 
 	internal GatewaySession CreateSession(
