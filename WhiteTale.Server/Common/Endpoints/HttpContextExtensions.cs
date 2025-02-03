87e7e6eb98c3b3c28d0ea2088db828f9f9bbd05e
Rex Aliis <rexaliisdev@gmail.com>
2025-02-03 12:23:10 -0300

    add HttpContext ext methods

---
+++ WhiteTale.Server/Common/Endpoints/HttpContextExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Http;
+
+namespace WhiteTale.Server.Common.Endpoints;
+
+internal static class HttpContextExtensions
+{
+	private const String SchemeDelimiter = "://";
+
+	internal static String GetUrl(this HttpRequest request)
+	{
+		var scheme = request.Scheme;
+		var host = request.Host.Value ?? String.Empty;
+		var pathBase = request.PathBase.Value ?? String.Empty;
+		var path = request.Path.Value ?? String.Empty;
+
+		var length = scheme.Length + SchemeDelimiter.Length + host.Length
+		             + pathBase.Length + path.Length;
+
+		return new StringBuilder(length)
+			.Append(scheme)
+			.Append(SchemeDelimiter)
+			.Append(host)
+			.Append(pathBase)
+			.Append(path)
+			.ToString();
+	}
+}
