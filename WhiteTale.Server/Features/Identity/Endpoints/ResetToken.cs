2364996f428f583f60de3a1f0bf2f9f55e8011dd
Rex Aliis <rexaliisdev@gmail.com>
2025-02-08 15:37:27 -0300

    add reset token endpoint

---
+++ WhiteTale.Server/Features/Identity/Endpoints/ResetToken.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace WhiteTale.Server.Features.Identity.Endpoints;
+
+internal sealed class ResetToken : IEndpoint
+{
+	public void Build(IEndpointRouteBuilder route)
+	{
+		_ = route.MapPost("identity/reset-token", HandleAsync)
+			.RequireRateLimiting(RateLimitPolicyNames.Global)
+			.HasApiVersion(1);
+	}
+
+	private static async Task<Results<NoContent, ProblemHttpResult, EmptyHttpResult>> HandleAsync(
+		[FromBody] LogInRequestBody body,
+		[FromServices] LogInRequestBodyValidator bodyValidator,
+		[FromServices] UserManager userManager,
+		[FromServices] ApplicationDbContext dbContext)
+	{
+		var bodyValidation = await bodyValidator.ValidateAsync(body);
+		if (!bodyValidation.IsValid)
+		{
+			var problemDetails = bodyValidation.Errors.ToProblemDetails();
+			return TypedResults.Problem(problemDetails);
+		}
+
+		var user = await userManager.FindByUserNameAsync(body.UserName);
+		if (user is null)
+		{
+			return TypedResults.Problem(ProblemDetailsDefaults.UnknownUser);
+		}
+
+		var isPasswordCorrect = userManager.CheckPassword(user, body.Password);
+		if (!isPasswordCorrect)
+		{
+			await userManager.AccessFailedAsync(user);
+			return TypedResults.Problem(ProblemDetailsDefaults.IncorrectPassword);
+		}
+
+		_ = dbContext.Attach(user);
+		user.UpdateSecurityStamp();
+		_ = dbContext.SaveChangesWithConcurrencyCheckBypassAsync();
+
+		return TypedResults.NoContent();
+	}
+}
