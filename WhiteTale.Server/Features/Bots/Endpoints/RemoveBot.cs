b3021bf4bd78ab8eb19c8115d00cbc316aa72cd0
Rex Aliis <rexaliisdev@gmail.com>
2025-02-09 15:26:32 -0300

    add remove bot endpoint

---
+++ WhiteTale.Server/Features/Bots/Endpoints/RemoveBot.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+
+namespace WhiteTale.Server.Features.Bots.Endpoints;
+
+internal sealed class RemoveBot : IEndpoint
+{
+	public void Build(IEndpointRouteBuilder route)
+	{
+		_ = route.MapDelete("bots/{userId}", HandleAsync)
+			.RequireRateLimiting(RateLimitPolicyNames.Global)
+			.RequireAuthenticatedUser()
+			.RequireUserType(UserType.Standard)
+			.RequirePermissions()
+			.DenyBannedUsers()
+			.HasApiVersion(1);
+	}
+
+	private static async Task<Results<NoContent, ProblemHttpResult, InternalServerError>> HandleAsync(
+		[FromRoute] UInt64 userId,
+		[FromServices] ApplicationDbContext dbContext)
+	{
+		var user = await dbContext.Users
+			.AsTracking()
+			.Where(u => u.Id == userId && u.Type == UserType.Bot && !u.IsRemoved)
+			.FirstOrDefaultAsync();
+		if (user is null)
+		{
+			return TypedResults.Problem(ProblemDetailsDefaults.UserDoesNotExist);
+		}
+
+		user.Remove();
+
+		try
+		{
+			_ = await dbContext.SaveChangesAsync();
+		}
+		catch (DbUpdateConcurrencyException)
+		{
+			return TypedResults.InternalServerError();
+		}
+
+		return TypedResults.NoContent();
+	}
+}
