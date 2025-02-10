096f803249c7b585b07f51f1fa25fe31104a9ab8
Rex Aliis <rexaliisdev@gmail.com>
2025-02-10 17:11:45 -0300

    paginate GetAllRooms and include connected rooms in response

--- WhiteTale.Server/Features/Rooms/Endpoints/GetAllRooms.cs
+++ WhiteTale.Server/Features/Rooms/Endpoints/GetAllRooms.cs
@@ -9,6 +9,12 @@ namespace WhiteTale.Server.Features.Rooms.Endpoints;
 
 internal sealed class GetAllRooms : IEndpoint
 {
+	private const String CountQueryParameter = "count";
+	internal const String AfterQueryParameter = "after";
+	internal const Int32 MinimumRoomCount = 2;
+	internal const Int32 MaximumRoomCount = 100;
+	private const Int32 DefaultRoomCount = 10;
+
 	public void Build(IEndpointRouteBuilder route)
 	{
 		_ = route.MapGet("rooms", HandleAsync)
@@ -18,22 +24,80 @@ internal sealed class GetAllRooms : IEndpoint
 			.HasApiVersion(1);
 	}
 
-	private static async Task<Ok<List<RoomData>>> HandleAsync([FromServices] ApplicationDbContext dbContext)
+	private static async Task<Results<Ok<List<RoomData>>, ProblemHttpResult>> HandleAsync(
+		[FromQuery(Name = CountQueryParameter)] Int32? count,
+		[FromQuery(Name = AfterQueryParameter)] UInt64? afterId,
+		[FromServices] ApplicationDbContext dbContext)
 	{
-		var rooms = await dbContext.Rooms
+		count ??= DefaultRoomCount;
+		if (count is > MaximumRoomCount or < MinimumRoomCount)
+		{
+			return TypedResults.Problem(ProblemDetailsDefaults.InvalidRoomCount);
+		}
+
+		var roomsQuery = dbContext.Rooms
 			.AsNoTracking()
 			.Where(r => !r.IsRemoved)
+			.OrderBy(r => r.CreationTime)
 			.Select(r =>
-				new RoomData
+				new
 				{
-					Id = r.Id,
-					Name = r.Name,
-					Description = r.Description,
-					IsEntrance = r.IsEntrance,
-					CreationTime = r.CreationTime,
+					r.Id,
+					r.Name,
+					r.Description,
+					r.IsEntrance,
+					r.CreationTime,
+				})
+			.Take((Int32)count);
+
+		if (afterId is not null)
+		{
+			var target = await dbContext.Rooms
+				.AsNoTracking()
+				.Where(r => r.Id == afterId && !r.IsRemoved)
+				.Select(r => new
+				{
+					r.CreationTime,
+				})
+				.FirstOrDefaultAsync();
+
+			if (target is null)
+			{
+				return TypedResults.Problem(ProblemDetailsDefaults.InvalidAfterRoom);
+			}
+
+			roomsQuery = roomsQuery.Where(r => r.CreationTime > target.CreationTime);
+		}
+
+		var rooms = await roomsQuery.ToListAsync();
+		var roomConnections = await dbContext.RoomConnections
+			.AsNoTracking()
+			.Where(c => rooms.Select(r => r.Id).Contains(c.SourceRoomId))
+			.Select(c =>
+				new
+				{
+					c.SourceRoomId,
+					c.TargetRoomId,
 				})
 			.ToListAsync();
 
-		return TypedResults.Ok(rooms);
+		List<RoomData> roomsData = new(rooms.Count);
+		foreach (var room in rooms)
+		{
+			roomsData.Add(new RoomData
+			{
+				Id = room.Id,
+				Name = room.Name,
+				Description = room.Description,
+				IsEntrance = room.IsEntrance,
+				ConnectedRoomIds = roomConnections
+					.Where(c => c.SourceRoomId == room.Id)
+					.Select(c => c.TargetRoomId)
+					.ToList(),
+				CreationTime = room.CreationTime,
+			});
+		}
+
+		return TypedResults.Ok(roomsData);
 	}
 }
---
+++ WhiteTale.Server/Features/Rooms/Endpoints/ProblemDetailsDefaults.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WhiteTale.Server.Features.Rooms.Endpoints;
+
+internal static class ProblemDetailsDefaults
+{
+	internal static ProblemDetails InvalidRoomCount { get; } = new()
+	{
+		Title = "Invalid message count.",
+		Detail = $"The message count must be between {GetAllRooms.MinimumRoomCount} and {GetAllRooms.MaximumRoomCount}.",
+		Status = StatusCodes.Status400BadRequest,
+	};
+
+	internal static ProblemDetails InvalidAfterRoom { get; } = new()
+	{
+		Title = $"Invalid '{GetAllRooms.AfterQueryParameter}' query parameter.",
+		Detail = "A message with the specified ID was not found.",
+		Status = StatusCodes.Status400BadRequest,
+	};
+}
