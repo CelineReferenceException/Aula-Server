3e251647218477c4afbefb924d31941e897bf213
Rex Aliis <rexaliisdev@gmail.com>
2025-02-11 12:51:35 -0300

    receive a more abstract parameter type

--- WhiteTale.Server/Common/Authorization/UserTypePolicyExtensions.cs
+++ WhiteTale.Server/Common/Authorization/UserTypePolicyExtensions.cs
@@ -8,12 +8,12 @@ internal static class UserTypePolicyExtensions
 {
 	private const String PolicyName = nameof(UserTypeRequirement);
 
-	internal static TBuilder RequireUserType<TBuilder>(this TBuilder builder, params IReadOnlyList<UserType> authorized)
+	internal static TBuilder RequireUserType<TBuilder>(this TBuilder builder, params IEnumerable<UserType> authorized)
 		where TBuilder : IEndpointConventionBuilder
 	{
 		_ = builder
 			.RequireAuthorization(PolicyName)
-			.WithMetadata(new RequireUserTypeAttribute(authorized));
+			.WithMetadata(new RequireUserTypeAttribute(authorized.ToArray()));
 		return builder;
 	}
 
