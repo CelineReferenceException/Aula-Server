d3dbbd796163d8b9b5d3bee2ca2c057bd645f804
Rex Aliis <rexaliisdev@gmail.com>
2025-02-11 19:06:07 -0300

    update send message req body validation messages

--- WhiteTale.Server/Features/Messages/Endpoints/SendMessageRequestBodyValidator.cs
+++ WhiteTale.Server/Features/Messages/Endpoints/SendMessageRequestBodyValidator.cs
@@ -13,14 +13,19 @@ internal sealed class SendMessageRequestBodyValidator : AbstractValidator<SendMe
 		_ = When(x => x.Type is MessageType.Standard, () =>
 		{
 			_ = RuleFor(x => x.Content)
-				.NotEmpty()
-				.WithErrorCode($"{nameof(SendMessageRequestBody.Content)} is empty")
-				.WithMessage($"{nameof(SendMessageRequestBody.Content)} cannot be empty.");
+				.NotNull()
+				.WithErrorCode($"{nameof(SendMessageRequestBody.Content)} is null")
+				.WithMessage($"{nameof(SendMessageRequestBody.Content)} cannot be null.");
+
+			_ = RuleFor(x => x.Content)
+				.MaximumLength(Message.ContentMinimumLength)
+				.WithErrorCode($"{nameof(SendMessageRequestBody.Content)} is too short")
+				.WithMessage($"{nameof(SendMessageRequestBody.Content)} length must be at least {Message.ContentMinimumLength}");
 
 			_ = RuleFor(x => x.Content)
 				.MaximumLength(Message.ContentMaximumLength)
-				.WithErrorCode($"{nameof(Message.Content)} is too long")
-				.WithMessage($"{nameof(Message.Content)} length must be at most {Message.ContentMaximumLength}");
+				.WithErrorCode($"{nameof(SendMessageRequestBody.Content)} is too long")
+				.WithMessage($"{nameof(SendMessageRequestBody.Content)} length must be at most {Message.ContentMaximumLength}");
 		});
 	}
 }
