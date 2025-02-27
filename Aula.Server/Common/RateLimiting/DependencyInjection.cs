37abe081fe7e3618d07f00f257d325a290b2f32f
Rex Aliis <rexaliisdev@gmail.com>
2025-02-27 03:04:04 -0300

    Update DependencyInjection.cs
    - Adapt old global ratelimit implementation
    - Remove zero-concurrency ratelimit implementation
    - Create dependency injection methods for the new ratelimit system
    - Create dependency injection methods to append endpoint metadata

--- Aula.Server/Common/RateLimiting/DependencyInjection.cs
+++ Aula.Server/Common/RateLimiting/DependencyInjection.cs
@@ -1,67 +1,174 @@
 using System.Threading.RateLimiting;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.RateLimiting;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Options;
 
 namespace Aula.Server.Common.RateLimiting;
 
 internal static class DependencyInjection
 {
-	internal static IServiceCollection AddRateLimiters(this IServiceCollection services)
+	internal static IServiceCollection AddCustomRateLimiter(
+		this IServiceCollection services,
+		Action<RateLimiterOptions> configureOptions)
 	{
-		_ = services.AddOptions<RateLimitOptions>(RateLimitPolicyNames.Global)
-			.BindConfiguration($"RateLimiters:{nameof(RateLimitPolicyNames.Global)}")
+		_ = services.AddCustomRateLimiter();
+		_ = services.Configure(configureOptions);
+		return services;
+	}
+
+	internal static IServiceCollection AddCustomRateLimiter(this IServiceCollection services)
+	{
+		services.TryAddSingleton<RateLimiterManager>();
+		_ = services.AddHostedService<RemoveUnusedRateLimitersHostedService>();
+
+		_ = services.AddOptions<RateLimitOptions>("Global")
+			.BindConfiguration("RateLimiters:Global")
 			.ValidateDataAnnotations()
 			.ValidateOnStart();
 
-		_ = services.PostConfigure<RateLimitOptions>(RateLimitPolicyNames.Global, options =>
+		_ = services.PostConfigure<RateLimitOptions>("Global", options =>
 		{
 			options.WindowMilliseconds ??= 1000;
 			options.PermitLimit ??= 30;
 		});
 
-		_ = services.AddRateLimiter(options =>
+		_ = services.Configure<RateLimiterOptions>(options =>
 		{
 			options.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
 
-			_ = options.AddPolicy(RateLimitPolicyNames.Global, httpContext =>
+			options.GlobalPolicy = new RateLimiterPolicy(httpContext =>
 			{
 				var userManager = ServiceProviderServiceExtensions.GetRequiredService<UserManager>(httpContext.RequestServices);
 
 				var userId = userManager.GetUserId(httpContext.User);
 				var partitionKey = userId.ToString() ?? httpContext.Connection.RemoteIpAddress?.ToString() ?? String.Empty;
 
-				var rateLimit = httpContext.RequestServices
-					.GetRequiredService<IOptionsSnapshot<RateLimitOptions>>()
-					.Get(RateLimitPolicyNames.Global);
-				var permitLimit = rateLimit.PermitLimit!.Value;
-				var window = TimeSpan.FromMilliseconds(rateLimit.WindowMilliseconds!.Value);
+				return RateLimitPartitionExtensions.GetExtendedFixedWindowRateLimiter(partitionKey, _ =>
+				{
+					var rateLimit = httpContext.RequestServices
+						.GetRequiredService<IOptionsSnapshot<RateLimitOptions>>()
+						.Get("Global");
 
-				return RateLimitPartition.GetFixedWindowLimiter(partitionKey,
-					_ => new FixedWindowRateLimiterOptions
+					return new FixedWindowRateLimiterOptions
 					{
-						PermitLimit = permitLimit,
-						Window = window,
-					});
-			});
-
-			_ = options.AddPolicy(RateLimitPolicyNames.NoConcurrency, httpContext =>
-			{
-				var userManager = ServiceProviderServiceExtensions.GetRequiredService<UserManager>(httpContext.RequestServices);
-				var request = httpContext.Request;
-
-				var userId = userManager.GetUserId(httpContext.User);
-				var partitionKey = userId.ToString() ?? httpContext.Connection.RemoteIpAddress?.ToString() ?? String.Empty;
-				partitionKey += $"{request.Method}{request.Scheme}{request.Host}{request.PathBase}{request.Path}";
-
-				return RateLimitPartition.GetConcurrencyLimiter(partitionKey,
-					_ => new ConcurrencyLimiterOptions
-					{
-						PermitLimit = 1,
-					});
-			});
+						PermitLimit = rateLimit.PermitLimit!.Value,
+						Window = TimeSpan.FromMilliseconds(rateLimit.WindowMilliseconds!.Value),
+					};
+				});
+			}, null);
 		});
 
 		return services;
+	}
+
+	internal static TBuilder UseCustomRateLimiting<TBuilder>(this TBuilder builder) where TBuilder : IApplicationBuilder
+	{
+		_ = builder.Use(async (httpContext, next) =>
+		{
+			var endpoint = httpContext.GetEndpoint();
+			if (endpoint is null)
+			{
+				_ = next(httpContext);
+				return;
+			}
+
+			var ignoreRateLimitingAttribute = endpoint.Metadata.GetMetadata<IgnoreRateLimitingAttribute>();
+			if (ignoreRateLimitingAttribute is not null)
+			{
+				_ = next(httpContext);
+				return;
+			}
+
+			var options = httpContext.RequestServices.GetRequiredService<IOptions<RateLimiterOptions>>()
+				.Value;
+			if (options.GlobalPolicy is not null)
+			{
+				var globalLease = await ExecuteRateLimiterPolicyAsync(options.GlobalPolicy, PolicyType.Global, httpContext);
+				if (!globalLease.IsAcquired)
+				{
+					httpContext.Response.StatusCode = options.RejectionStatusCode;
+					return;
+				}
+			}
+
+			var rateLimit = endpoint.Metadata.GetMetadata<RequireRateLimitingAttribute>();
+			if (rateLimit is null)
+			{
+				_ = next(httpContext);
+				return;
+			}
+
+			if (!options.PolicyMap.TryGetValue(rateLimit.PolicyName, out var policy))
+			{
+				throw new InvalidOperationException(
+					$"This endpoint requires a rate limiting policy with name {rateLimit.PolicyName}, but no such policy exists.");
+			}
+
+			var lease = await ExecuteRateLimiterPolicyAsync(policy, PolicyType.Scope, httpContext);
+			if (!lease.IsAcquired)
+			{
+				httpContext.Response.StatusCode = options.RejectionStatusCode;
+				return;
+			}
+
+			_ = next(httpContext);
+		});
+
+		return builder;
+	}
+
+	internal static TBuilder IgnoreRateLimiting<TBuilder>(this TBuilder builder) where TBuilder : IEndpointConventionBuilder
+	{
+		return builder.WithMetadata(new IgnoreRateLimitingAttribute());
+	}
+
+	internal static TBuilder ApplyRateLimiting<TBuilder>(this TBuilder builder, String policyName)
+		where TBuilder : IEndpointConventionBuilder
+	{
+		return builder.WithMetadata(new RequireRateLimitingAttribute(policyName));
+	}
+
+	private static async ValueTask<RateLimitLease> ExecuteRateLimiterPolicyAsync(
+		RateLimiterPolicy policy,
+		PolicyType type,
+		HttpContext httpContext)
+	{
+		var rateLimiterManager = httpContext.RequestServices.GetRequiredService<RateLimiterManager>();
+		var rateLimiter = rateLimiterManager.GetOrAdd(policy.GetPartition(httpContext));
+
+		var rateLimitLease = rateLimiter.AttemptAcquire();
+
+		var statistics = rateLimiter.GetStatistics();
+		if (statistics is not null)
+		{
+			httpContext.Response.Headers.Append($"X-RateLimit-{type}-Remaining",
+				statistics.CurrentAvailablePermits.ToString());
+		}
+
+		if (rateLimiter is ExtendedReplenishingRateLimiter { ReplenishmentDateTime: not null, } replenishingRateLimiter)
+		{
+			var replenishmentDateTime = replenishingRateLimiter.ReplenishmentDateTime.Value;
+			httpContext.Response.Headers.Append($"X-RateLimit-{type}-ResetDateTime", replenishmentDateTime.ToString("O"));
+		}
+
+		if (!rateLimitLease.IsAcquired &&
+		    policy.OnRejected is not null)
+		{
+			await policy.OnRejected(new OnRejectedContext
+			{
+				HttpContext = httpContext,
+				Lease = rateLimitLease,
+			}, httpContext.RequestAborted);
+		}
+
+		return rateLimitLease;
+	}
+
+	private enum PolicyType
+	{
+		Global,
+		Scope,
 	}
 }
