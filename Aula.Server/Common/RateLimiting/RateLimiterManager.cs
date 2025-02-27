5c26bf58d196f5757723ccbfa49db462b7a217f9
Rex Aliis <rexaliisdev@gmail.com>
2025-02-27 02:57:30 -0300

    add ratelimit manager
    caches rate limiters

---
+++ Aula.Server/Common/RateLimiting/RateLimiterManager.cs
@@ -0,0 +1,32 @@
+using System.Collections.Concurrent;
+using System.Threading.RateLimiting;
+
+namespace Aula.Server.Common.RateLimiting;
+
+internal sealed class RateLimiterManager
+{
+	private readonly ConcurrentDictionary<String, RateLimiter> _rateLimiters = new();
+
+	internal TimeSpan MaximumReplenishmentPeriod { get; private set; } = TimeSpan.Zero;
+
+	internal RateLimiter GetOrAdd(RateLimitPartition<String> partition)
+	{
+		var rateLimiter = _rateLimiters.GetOrAdd(partition.PartitionKey, static (_, p) => p.Factory(p.PartitionKey), partition);
+		if (rateLimiter is ReplenishingRateLimiter replenishingRateLimiter &&
+		    replenishingRateLimiter.ReplenishmentPeriod > MaximumReplenishmentPeriod)
+		{
+			MaximumReplenishmentPeriod = replenishingRateLimiter.ReplenishmentPeriod;
+		}
+
+		return rateLimiter;
+	}
+
+	internal void RemoveUnusedRateLimiters(TimeSpan idleDuration)
+	{
+		foreach (var entry in _rateLimiters.Where(r => r.Value.IdleDuration > idleDuration))
+		{
+			entry.Value.Dispose();
+			_ = _rateLimiters.TryRemove(entry);
+		}
+	}
+}
