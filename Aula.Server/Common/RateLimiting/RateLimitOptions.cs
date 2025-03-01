3f4e2cbfff1cc0392efa145c0da441272a8c4b9c
Rex Aliis <rexaliisdev@gmail.com>
2025-03-01 13:28:17 -0300

    validate the value range

--- Aula.Server/Common/RateLimiting/RateLimitOptions.cs
+++ Aula.Server/Common/RateLimiting/RateLimitOptions.cs
@@ -1,4 +1,6 @@
-﻿namespace Aula.Server.Common.RateLimiting;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Aula.Server.Common.RateLimiting;
 
 /// <summary>
 ///     Configurations for a rate limit profile.
@@ -8,10 +10,12 @@ internal sealed class RateLimitOptions
 	/// <summary>
 	///     The window of the rate limit in milliseconds.
 	/// </summary>
+	[Range(0, Int32.MaxValue)]
 	public Int32? WindowMilliseconds { get; set; }
 
 	/// <summary>
 	///     How many requests can be made during the rate limit window.
 	/// </summary>
+	[Range(0, Int32.MaxValue)]
 	public Int32? PermitLimit { get; set; }
 }
