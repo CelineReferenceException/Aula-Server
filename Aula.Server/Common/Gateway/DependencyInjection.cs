30871d0424c7a8754e3d7c91770e2387cafcd5e9
Rex Aliis <rexaliisdev@gmail.com>
2025-03-13 19:05:40 -0300

    add header extractor middleware
    The [WebSocket](https://developer.mozilla.org/en-US/docs/Web/API/WebSocket) API does not allow passing headers other than Sec-WebSocket-Protocol, so I'm going to pass these headers inside the protocol header as a JSON object, which I can extract and read later with this middleware.

--- Aula.Server/Common/Gateway/DependencyInjection.cs
+++ Aula.Server/Common/Gateway/DependencyInjection.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.WebSockets;
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.WebSockets;
+using Microsoft.Extensions.Primitives;
 
 namespace Aula.Server.Common.Gateway;
 
@@ -21,5 +25,52 @@ internal static class DependencyInjection
 		});
 
 		return services;
+	}
+
+	internal static TBuilder ExtractHeadersFromWebSocketProtocols<TBuilder>(this TBuilder builder) where TBuilder : IApplicationBuilder
+	{
+		_ = builder.Use((httpContext, next) =>
+		{
+			if (!httpContext.Request.Headers.TryGetValue("Sec-WebSocket-Protocol", out var protocols) ||
+			    protocols.Count == 0)
+			{
+				return next(httpContext);
+			}
+
+			for (var i = 0; i < protocols.Count; i++)
+			{
+				var protocol = protocols[i];
+
+				if (protocol == null ||
+				    !protocol.StartsWith('{') ||
+				    !protocol.EndsWith('}'))
+				{
+					continue;
+				}
+
+				try
+				{
+					var headers = JsonSerializer.Deserialize<Dictionary<String, String>>(protocol)!;
+					foreach (var header in headers)
+					{
+						httpContext.Request.Headers.Append(header.Key, header.Value);
+					}
+
+					var remainingProtocols = protocols.ToArray().ToList();
+					remainingProtocols.RemoveAt(i);
+
+					httpContext.Request.Headers.SecWebSocketProtocol = new StringValues([.. remainingProtocols,]);
+					break;
+				}
+				catch (JsonException)
+				{
+					// Prevent stopping if the received JSON was invalid.
+				}
+			}
+
+			return next(httpContext);
+		});
+
+		return builder;
 	}
 }
