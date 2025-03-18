a44ff2456ffbf5f03f2b5d412565cb5f0ea4d47a
Rex Aliis <rexaliisdev@gmail.com>
2025-03-18 00:24:08 -0300

    remove unnecessary method

--- Aula.Server/Common/Endpoints/ProblemDetailsExtensions.cs
+++ Aula.Server/Common/Endpoints/ProblemDetailsExtensions.cs
@@ -1,6 +1,5 @@
 ﻿using FluentValidation.Results;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
 
 namespace Aula.Server.Common.Endpoints;
 
@@ -17,21 +16,6 @@ internal static class ProblemDetailsExtensions
 			Status = StatusCodes.Status400BadRequest,
 			Title = "Validation problem",
 			Detail = "One or more validation errors occurred.",
-			Errors = problemErrors,
-		};
-	}
-
-	internal static HttpValidationProblemDetails ToProblemDetails(this IEnumerable<IdentityError> identityErrors)
-	{
-		var problemErrors = identityErrors
-			.Select(static error => new KeyValuePair<String, String[]>(error.Code, [error.Description,]))
-			.ToDictionary();
-
-		return new HttpValidationProblemDetails
-		{
-			Status = StatusCodes.Status400BadRequest,
-			Title = "Identity problem",
-			Detail = "One or more identity errors occurred.",
 			Errors = problemErrors,
 		};
 	}
