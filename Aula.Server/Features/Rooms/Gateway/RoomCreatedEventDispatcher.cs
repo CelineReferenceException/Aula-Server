9fdbd10e55e38a516a1ab40ab8fe7b40ccd48c8d
Rex Aliis <rexaliisdev@gmail.com>
2025-03-19 19:50:54 -0300

    rename class

--- Aula.Server/Features/Rooms/Gateway/RoomCreatedEventDispatcher.cs
+++ Aula.Server/Features/Rooms/Gateway/RoomCreatedEventDispatcher.cs
@@ -5,12 +5,12 @@ using Microsoft.Extensions.Options;
 
 namespace Aula.Server.Features.Rooms.Gateway;
 
-internal sealed class RoomCreatedEventHandler : INotificationHandler<RoomCreatedEvent>
+internal sealed class RoomCreatedEventDispatcher : INotificationHandler<RoomCreatedEvent>
 {
 	private readonly GatewayService _gatewayService;
 	private readonly JsonSerializerOptions _jsonSerializerOptions;
 
-	public RoomCreatedEventHandler(IOptions<JsonOptions> jsonOptions, GatewayService gatewayService)
+	public RoomCreatedEventDispatcher(IOptions<JsonOptions> jsonOptions, GatewayService gatewayService)
 	{
 		_gatewayService = gatewayService;
 		_jsonSerializerOptions = jsonOptions.Value.SerializerOptions;
