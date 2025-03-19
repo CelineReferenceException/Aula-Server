96bca8c64df0647a4ae58ee7d8636e6e7ecd3420
Rex Aliis <rexaliisdev@gmail.com>
2025-03-19 13:37:35 -0300

    combine if statements

--- Aula.Server/Features/Messages/Gateway/MessageCreatedEventHandler.cs
+++ Aula.Server/Features/Messages/Gateway/MessageCreatedEventHandler.cs
@@ -71,12 +71,8 @@ internal sealed class MessageCreatedEventHandler : INotificationHandler<MessageC
 
 		foreach (var session in _gatewayService.Sessions.Values)
 		{
-			if (!session.Intents.HasFlag(Intents.Messages))
-			{
-				continue;
-			}
-
-			if (!sessionUsers.TryGetValue(session.UserId, out var user) ||
+			if (!session.Intents.HasFlag(Intents.Messages) ||
+			    !sessionUsers.TryGetValue(session.UserId, out var user) ||
 			    user.CurrentRoomId is null ||
 			    !user.Permissions.HasFlag(Permissions.Administrator))
 			{
