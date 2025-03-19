6f80f24c20413d090267fc9422c993b864258e12
Rex Aliis <rexaliisdev@gmail.com>
2025-03-19 01:52:32 -0300

    remove method override

--- Aula.Server/Common/JsonSerialization/ObjectExtensions.cs
+++ Aula.Server/Common/JsonSerialization/ObjectExtensions.cs
@@ -4,11 +4,6 @@ namespace Aula.Server.Common.JsonSerialization;
 
 internal static class ObjectExtensions
 {
-	internal static Byte[] GetJsonUtf8Bytes<TValue>(this TValue value)
-	{
-		return GetJsonUtf8Bytes(value, JsonSerializerOptions.Web);
-	}
-
 	internal static Byte[] GetJsonUtf8Bytes<TValue>(this TValue value, JsonSerializerOptions options)
 	{
 		return JsonSerializer.SerializeToUtf8Bytes(value, options);
