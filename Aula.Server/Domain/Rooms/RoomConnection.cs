b555c7169319b7bff0a92c91f5f416ceaca83587
Rex Aliis <rexaliisdev@gmail.com>
2025-03-21 08:47:17 -0300

    replace UInt64 usage with Snowflake

--- Aula.Server/Domain/Rooms/RoomConnection.cs
+++ Aula.Server/Domain/Rooms/RoomConnection.cs
@@ -4,39 +4,24 @@ namespace Aula.Server.Domain.Rooms;
 
 internal sealed class RoomConnection : DefaultDomainEntity
 {
-	internal UInt64 Id { get; }
+	internal Snowflake Id { get; }
 
-	internal UInt64 SourceRoomId { get; }
+	internal Snowflake SourceRoomId { get; }
 
 	// Navigation property, values are set through reflection.
 	[MaybeNull]
 	[SuppressMessage("ReSharper", "UnassignedGetOnlyAutoProperty")]
 	internal Room SourceRoom { get; }
 
-	internal UInt64 TargetRoomId { get; }
+	internal Snowflake TargetRoomId { get; }
 
 	// Navigation property, values are set through reflection.
 	[MaybeNull]
 	[SuppressMessage("ReSharper", "UnassignedGetOnlyAutoProperty")]
 	internal Room TargetRoom { get; }
 
-	internal RoomConnection(UInt64 id, UInt64 sourceRoomId, UInt64 targetRoomId)
+	internal RoomConnection(Snowflake id, Snowflake sourceRoomId, Snowflake targetRoomId)
 	{
-		if (id is 0)
-		{
-			throw new ArgumentException($"{nameof(id)} cannot be 0.", nameof(id));
-		}
-
-		if (sourceRoomId is 0)
-		{
-			throw new ArgumentException($"{nameof(sourceRoomId)} cannot be 0.", nameof(sourceRoomId));
-		}
-
-		if (targetRoomId is 0)
-		{
-			throw new ArgumentException($"{nameof(targetRoomId)} cannot be 0.", nameof(targetRoomId));
-		}
-
 		Id = id;
 		SourceRoomId = sourceRoomId;
 		TargetRoomId = targetRoomId;
