07ea6fd40759ec435b9e2aaf0af8f0004593d5b4
Rex Aliis <rexaliisdev@gmail.com>
2025-03-20 22:46:55 -0300

    rewrite how messages  are instantiated

--- Aula.Server/Domain/Messages/Message.cs
+++ Aula.Server/Domain/Messages/Message.cs
@@ -1,4 +1,4 @@
-﻿using FluentValidation;
+﻿using System.Diagnostics;
 
 namespace Aula.Server.Domain.Messages;
 
@@ -8,11 +8,6 @@ internal sealed class Message : DefaultDomainEntity
 
 	internal const Int32 ContentMinimumLength = 1;
 	internal const Int32 ContentMaximumLength = 2048;
-	private static readonly MessageValidator s_validator = new();
-
-	private Message()
-	{
-	}
 
 	internal UInt64 Id { get; private init; }
 
@@ -36,7 +31,7 @@ internal sealed class Message : DefaultDomainEntity
 
 	internal Boolean IsRemoved { get; private set; }
 
-	internal static Message Create(
+	internal Message(
 		UInt64 id,
 		MessageType type,
 		MessageFlags flags,
@@ -47,41 +42,84 @@ internal sealed class Message : DefaultDomainEntity
 		MessageUserLeave? leaveData,
 		UInt64 roomId)
 	{
-		var allowedFlags = type switch
+		if (id is 0)
 		{
-			MessageType.Standard => StandardTypeAllowedFlags,
-			MessageType.UserJoin => (MessageFlags)0,
-			MessageType.UserLeave => (MessageFlags)0,
-			_ => throw new InvalidOperationException($"Invalid {nameof(MessageType)} value: '{type}')"),
-		};
+			throw new ArgumentException($"{nameof(id)} cannot be 0.", nameof(id));
+		}
 
 		if (flags > 0)
 		{
+			var allowedFlags = type switch
+			{
+				MessageType.Standard => StandardTypeAllowedFlags,
+				MessageType.UserJoin => (MessageFlags)0,
+				MessageType.UserLeave => (MessageFlags)0,
+				_ => throw new UnreachableException($"No case defined for {nameof(MessageType)}.{type}"),
+			};
+
 			flags = flags
 				.GetFlags()
 				.Where(flag => allowedFlags.HasFlag(flag))
 				.Aggregate((x, y) => x | y);
 		}
 
-		var message = new Message
+		if (authorType is MessageAuthorType.User &&
+		    authorId is null)
 		{
-			Id = id,
-			Type = type,
-			Flags = flags,
-			AuthorType = authorType,
-			AuthorId = authorId,
-			RoomId = roomId,
-			Content = content,
-			JoinData = joinData,
-			LeaveData = leaveData,
-			CreationDate = DateTime.UtcNow,
-		};
+			throw new ArgumentException($"{nameof(authorId)} cannot be null when {nameof(authorType)} is {MessageAuthorType.User}.",
+				nameof(authorId));
+		}
 
-		s_validator.ValidateAndThrow(message);
+		if (authorId is 0)
+		{
+			throw new ArgumentException($"{nameof(id)} cannot be 0.", nameof(id));
+		}
 
-		message.AddEvent(new MessageCreatedEvent(message));
+		if (content is not null)
+		{
+			switch (content.Length)
+			{
+				case < ContentMinimumLength:
+					throw new ArgumentOutOfRangeException(nameof(content),
+						$"{nameof(content)} length must be at least {ContentMinimumLength}.");
+				case > ContentMaximumLength:
+					throw new ArgumentOutOfRangeException(nameof(content),
+						$"{nameof(content)} length must be at most ${ContentMaximumLength}.");
+				default: break;
+			}
+		}
 
-		return message;
+		if (joinData is null &&
+		    type is MessageType.UserJoin)
+		{
+			throw new ArgumentException($"{nameof(joinData)} cannot be null when {nameof(type)} is {MessageType.UserJoin}.",
+				nameof(joinData));
+		}
+
+		if (leaveData is null &&
+		    type is MessageType.UserLeave)
+		{
+			throw new ArgumentException($"{nameof(leaveData)} cannot be null when {nameof(type)} is {MessageType.UserLeave}.",
+				nameof(leaveData));
+		}
+
+		if (roomId is 0)
+		{
+			throw new ArgumentException($"{nameof(roomId)} cannot be 0.", nameof(roomId));
+		}
+
+		Id = id;
+		Type = type;
+		Flags = flags;
+		AuthorType = authorType;
+		AuthorId = authorId;
+		RoomId = roomId;
+		Content = content;
+		JoinData = joinData;
+		LeaveData = leaveData;
+		CreationDate = DateTime.UtcNow;
+
+		AddEvent(new MessageCreatedEvent(this));
 	}
 
 	internal void Remove()
