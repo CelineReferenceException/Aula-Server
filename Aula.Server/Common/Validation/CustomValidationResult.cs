fa776b43bc028409b3c8f7a41b6a8ed5aaacf18d
Rex Aliis <rexaliisdev@gmail.com>
2025-03-20 21:27:46 -0300

    add a custom validation result with success value

--- Aula.Server/Common/Validation/CustomValidationResult.cs
+++ Aula.Server/Common/Validation/CustomValidationResult.cs
@@ -32,3 +32,44 @@ internal readonly struct CustomValidationResult : IEquatable<CustomValidationRes
 
 	public static Boolean operator !=(CustomValidationResult left, CustomValidationResult right) => !left.Equals(right);
 }
+
+internal readonly struct CustomValidationResult<TResult> : IEquatable<CustomValidationResult<TResult>>
+{
+	internal TResult? Result { get; }
+
+	internal CustomValidationProblemValues Problems { get; }
+
+	internal Boolean Succeeded => Problems.Count is 0;
+
+	internal CustomValidationResult(TResult result)
+	{
+		Result = result;
+	}
+
+	internal CustomValidationResult(CustomValidationProblemValues problems)
+	{
+		Problems = problems;
+	}
+
+	public Boolean Equals(CustomValidationResult<TResult> other)
+	{
+		return Succeeded == other.Succeeded &&
+		       Problems.Equals(other.Problems) &&
+		       ((Result is not null && Result.Equals(other.Result)) ||
+		        (Result is null && other.Result is null));
+	}
+
+	public override Boolean Equals(Object? obj)
+	{
+		return obj is CustomValidationResult<TResult> other && Equals(other);
+	}
+
+	public override Int32 GetHashCode()
+	{
+		return HashCode.Combine(Succeeded, Result, Problems);
+	}
+
+	public static Boolean operator ==(CustomValidationResult<TResult> left, CustomValidationResult<TResult> right) => left.Equals(right);
+
+	public static Boolean operator !=(CustomValidationResult<TResult> left, CustomValidationResult<TResult> right) => !left.Equals(right);
+}
