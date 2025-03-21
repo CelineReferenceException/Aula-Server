da808bf78cf8da7b13912c68a01c945573c767a4
Rex Aliis <rexaliisdev@gmail.com>
2025-03-21 02:16:14 -0300

    initialize properties through constructor

--- Aula.Server/Domain/Messages/MessageUserLeave.cs
+++ Aula.Server/Domain/Messages/MessageUserLeave.cs
@@ -4,13 +4,35 @@ namespace Aula.Server.Domain.Messages;
 
 internal sealed record MessageUserLeave
 {
-	internal required UInt64 MessageId { get; init; }
+	internal UInt64 MessageId { get; }
 
 	// Navigation property, values are set through reflection.
 	[SuppressMessage("ReSharper", "UnassignedGetOnlyAutoProperty")]
 	internal Message Message { get; } = null!;
 
-	internal required UInt64 UserId { get; init; }
+	internal UInt64 UserId { get; }
 
-	internal UInt64? RoomId { get; init; }
+	internal UInt64? RoomId { get; }
+
+	internal MessageUserLeave(UInt64 messageId, UInt64 userId, UInt64? roomId)
+	{
+		if (messageId is 0)
+		{
+			throw new ArgumentException($"{nameof(messageId)} cannot be 0.", nameof(messageId));
+		}
+
+		if (userId is 0)
+		{
+			throw new ArgumentException($"{nameof(userId)} cannot be 0.", nameof(userId));
+		}
+
+		if (roomId is 0)
+		{
+			throw new ArgumentException($"{nameof(roomId)} cannot be 0.", nameof(roomId));
+		}
+
+		MessageId = messageId;
+		UserId = userId;
+		RoomId = roomId;
+	}
 }
