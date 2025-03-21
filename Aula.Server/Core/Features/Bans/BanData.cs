2701b8a61f1224961525841bbf2f7a1d66f0e028
Rex Aliis <rexaliisdev@gmail.com>
2025-03-21 09:05:29 -0300

    replace UInt64 usage with Snowflake

--- Aula.Server/Core/Features/Bans/BanData.cs
+++ Aula.Server/Core/Features/Bans/BanData.cs
@@ -4,11 +4,11 @@ internal sealed record BanData
 {
 	public required BanType Type { get; init; }
 
-	public UInt64? ExecutorId { get; init; }
+	public Snowflake? ExecutorId { get; init; }
 
 	public String? Reason { get; init; }
 
-	public UInt64? TargetId { get; init; }
+	public Snowflake? TargetId { get; init; }
 
 	public required DateTime CreationDate { get; init; }
 }
