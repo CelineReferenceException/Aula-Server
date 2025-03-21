eddc74bfc574fe6b2302cef7838a640fc94ffdad
Rex Aliis <rexaliisdev@gmail.com>
2025-03-21 08:47:24 -0300

    replace UInt64 usage with Snowflake

--- Aula.Server/Domain/Messages/MessageUserLeave.cs
+++ Aula.Server/Domain/Messages/MessageUserLeave.cs
@@ -4,32 +4,17 @@ namespace Aula.Server.Domain.Messages;
 
 internal sealed record MessageUserLeave
 {
-	internal UInt64 MessageId { get; }
+	internal Snowflake MessageId { get; }
 
 	// Navigation property, values are set through reflection.
 	internal Message? Message { get; init; }
 
-	internal UInt64 UserId { get; }
+	internal Snowflake UserId { get; }
 
-	internal UInt64? RoomId { get; }
+	internal Snowflake? RoomId { get; }
 
-	internal MessageUserLeave(UInt64 messageId, UInt64 userId, UInt64? roomId)
+	internal MessageUserLeave(Snowflake messageId, Snowflake userId, Snowflake? roomId)
 	{
-		if (messageId is 0)
-		{
-			throw new ArgumentException($"{nameof(messageId)} cannot be 0.", nameof(messageId));
-		}
-
-		if (userId is 0)
-		{
-			throw new ArgumentException($"{nameof(userId)} cannot be 0.", nameof(userId));
-		}
-
-		if (roomId is 0)
-		{
-			throw new ArgumentException($"{nameof(roomId)} cannot be 0.", nameof(roomId));
-		}
-
 		MessageId = messageId;
 		UserId = userId;
 		RoomId = roomId;
