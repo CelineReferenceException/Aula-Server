b7e442e427fee1be5c6efd332fdd0f19ec7c65ce
Rex Aliis <rexaliisdev@gmail.com>
2025-03-21 02:16:10 -0300

    initialize properties through constructor

--- Aula.Server/Domain/Messages/MessageUserJoin.cs
+++ Aula.Server/Domain/Messages/MessageUserJoin.cs
@@ -4,11 +4,27 @@ namespace Aula.Server.Domain.Messages;
 
 internal sealed record MessageUserJoin
 {
-	internal required UInt64 MessageId { get; init; }
+	internal UInt64 MessageId { get; }
 
 	// Navigation property, values are set through reflection.
 	[SuppressMessage("ReSharper", "UnassignedGetOnlyAutoProperty")]
 	internal Message Message { get; } = null!;
 
-	internal required UInt64 UserId { get; init; }
+	internal UInt64 UserId { get; }
+
+	internal MessageUserJoin(UInt64 messageId, UInt64 userId)
+	{
+		if (messageId is 0)
+		{
+			throw new ArgumentException($"{nameof(messageId)} cannot be 0.", nameof(messageId));
+		}
+
+		if (userId is 0)
+		{
+			throw new ArgumentException($"{nameof(userId)} cannot be 0.", nameof(userId));
+		}
+
+		MessageId = messageId;
+		UserId = userId;
+	}
 }
