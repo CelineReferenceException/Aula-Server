dd0607951e700fe36133417230c9b9e52f333a46
Rex Aliis <rexaliisdev@gmail.com>
2025-03-21 03:04:09 -0300

    add method to check if all flags in an enum value are defined

--- Aula.Server/Common/EnumExtensions.cs
+++ Aula.Server/Common/EnumExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace Aula.Server.Common;
+﻿using System.Runtime.CompilerServices;
+
+namespace Aula.Server.Common;
 
 internal static class EnumExtensions
 {
@@ -6,5 +8,29 @@ internal static class EnumExtensions
 	{
 		var allFlags = Enum.GetValues<TEnum>();
 		return allFlags.Where(flag => e.HasFlag(flag));
+	}
+
+	internal static Boolean IsEnumFlagDefined<TEnum>(this TEnum value) where TEnum : struct, Enum
+	{
+		return IsEnumFlagDefined(Unsafe.As<TEnum, Int64>(ref value), typeof(TEnum));
+	}
+
+	private static Boolean IsEnumFlagDefined(Int64 value, Type enumType)
+	{
+		Int64 confirmedFlags = 0;
+		foreach (var enumFlag in Enum.GetValues(enumType))
+		{
+			var flagInt = Convert.ToInt64(enumFlag);
+			if ((flagInt & value) == flagInt)
+			{
+				confirmedFlags |= flagInt;
+				if (confirmedFlags == value)
+				{
+					return true;
+				}
+			}
+		}
+
+		return false;
 	}
 }
