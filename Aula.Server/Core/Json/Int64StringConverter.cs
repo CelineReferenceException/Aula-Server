81815e89d97c73594f4e7453ea2bce90003bf190
Rex Aliis <rexaliisdev@gmail.com>
2025-03-22 02:36:31 -0300

    accept only json string values

--- Aula.Server/Core/Json/Int64StringConverter.cs
+++ Aula.Server/Core/Json/Int64StringConverter.cs
@@ -9,12 +9,13 @@ internal sealed class Int64StringConverter : JsonConverter<Int64>
 	[SuppressMessage("Style", "IDE0072:Add missing cases")]
 	public override Int64 Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
 	{
-		return reader.TokenType switch
+		if (reader.TokenType is not JsonTokenType.String ||
+		    !Int64.TryParse(reader.ValueSpan, out var result))
 		{
-			JsonTokenType.Number => reader.GetInt64(),
-			JsonTokenType.String => Int64.Parse(reader.ValueSpan),
-			_ => throw new JsonException(),
-		};
+			throw new JsonException();
+		}
+
+		return result;
 	}
 
 	public override void Write(Utf8JsonWriter writer, Int64 value, JsonSerializerOptions options)
