9565a09d5361b330245d41d713b7634556d2fe89
Rex Aliis <rexaliisdev@gmail.com>
2025-03-22 21:30:30 -0300

    rewrite room connection model

--- Aula.Server/Domain/Rooms/RoomConnection.cs
+++ Aula.Server/Domain/Rooms/RoomConnection.cs
@@ -1,32 +1,40 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-namespace Aula.Server.Domain.Rooms;
+﻿namespace Aula.Server.Domain.Rooms;
 
 internal sealed class RoomConnection : DefaultDomainEntity
 {
-	internal Snowflake Id { get; }
+	private readonly Room? _sourceRoom;
+	private readonly Room? _targetRoom;
 
-	internal Snowflake SourceRoomId { get; }
-
-	// Navigation property, values are set through reflection.
-	[MaybeNull]
-	[SuppressMessage("ReSharper", "UnassignedGetOnlyAutoProperty", Justification = "Navigation property accessed through reflection")]
-	internal Room SourceRoom { get; }
-
-	internal Snowflake TargetRoomId { get; }
-
-	// Navigation property, values are set through reflection.
-	[MaybeNull]
-	[SuppressMessage("ReSharper", "UnassignedGetOnlyAutoProperty", Justification = "Navigation property accessed through reflection")]
-	internal Room TargetRoom { get; }
-
-	internal RoomConnection(Snowflake id, Snowflake sourceRoomId, Snowflake targetRoomId)
+	private RoomConnection(Snowflake id, Snowflake sourceRoomId, Snowflake targetRoomId)
 	{
 		Id = id;
 		SourceRoomId = sourceRoomId;
 		TargetRoomId = targetRoomId;
+	}
 
-		AddEvent(new RoomConnectionCreatedEvent(this));
+	internal Snowflake Id { get; }
+
+	internal Snowflake SourceRoomId { get; }
+
+	internal Snowflake TargetRoomId { get; }
+
+	internal Room SourceRoom
+	{
+		get => _sourceRoom ?? throw new InvalidOperationException("SourceRoom is null");
+		init => _sourceRoom = value;
+	}
+
+	internal Room TargetRoom
+	{
+		get => _targetRoom ?? throw new InvalidOperationException("TargetRoom is null");
+		init => _targetRoom = value;
+	}
+
+	internal static Result<RoomConnection> Create(Snowflake id, Snowflake sourceRoomId, Snowflake targetRoomId)
+	{
+		var roomConnection = new RoomConnection(id, sourceRoomId, targetRoomId);
+		roomConnection.AddEvent(new RoomConnectionCreatedEvent(roomConnection));
+		return roomConnection;
 	}
 
 	internal void Remove()
