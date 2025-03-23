03eeaadc8c3447a43b69d3dcc652ee9c67477be2
Rex Aliis <rexaliisdev@gmail.com>
2025-03-23 16:09:40 -0300

    add Result to ValidationProblemDetails extension method

--- Aula.Server/Core/Endpoints/ProblemDetailsExtensions.cs
+++ Aula.Server/Core/Endpoints/ProblemDetailsExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation.Results;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Aula.Server.Core.Endpoints;
 
@@ -17,6 +18,31 @@ internal static class ProblemDetailsExtensions
 			Title = "Validation problem",
 			Detail = "One or more validation errors occurred.",
 			Errors = problemErrors,
+		};
+	}
+
+	internal static ValidationProblemDetails ToProblemDetails(this Result<Object> result)
+	{
+		var propertyProblems = new Dictionary<String, List<String>>();
+
+		foreach (var problem in result.Problems)
+		{
+			if (propertyProblems.TryGetValue(problem.Name, out var problems))
+			{
+				problems.Add(problem.Description);
+			}
+			else
+			{
+				propertyProblems.Add(problem.Name, [problem.Description,]);
+			}
+		}
+
+		return new ValidationProblemDetails
+		{
+			Status = StatusCodes.Status400BadRequest,
+			Title = "Validation problem",
+			Detail = "One or more validation errors occurred.",
+			Errors = propertyProblems.ToDictionary(kvp => kvp.Key, kvp => kvp.Value.ToArray(), StringComparer.OrdinalIgnoreCase),
 		};
 	}
 }
