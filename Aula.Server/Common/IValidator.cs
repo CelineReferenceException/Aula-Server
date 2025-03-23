a28a51066d7b80a6e6fba803f237fa42aa35a7ca
Rex Aliis <rexaliisdev@gmail.com>
2025-03-23 16:10:26 -0300

    add TProblem type param

--- Aula.Server/Common/IValidator.cs
+++ Aula.Server/Common/IValidator.cs
@@ -1,6 +1,7 @@
 namespace Aula.Server.Common;
 
-internal interface IValidator<in TObject>
+internal interface IValidator<in TObject, TProblem>
+	where TProblem : class?
 {
 	Result Validate(TObject obj);
 }
