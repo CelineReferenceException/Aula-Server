4517e166c2cb82654fd94e6e6e38b2583572157d
Rex Aliis <rexaliisdev@gmail.com>
2025-03-24 19:00:39 -0300

    Revert "Delete RoomValidator.cs"
    This reverts commit ac6a8343100f30d599dc6a819ebdc615abd41b09.

---
+++ Aula.Server/Domain/Rooms/RoomValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace Aula.Server.Domain.Rooms;
+
+internal sealed class RoomValidator : AbstractValidator<Room>
+{
+	public RoomValidator()
+	{
+		_ = RuleFor(x => x.Name)
+			.NotNull()
+			.MinimumLength(Room.NameMinimumLength)
+			.MaximumLength(Room.NameMaximumLength);
+
+		_ = RuleFor(x => x.Description)
+			.NotNull()
+			.MaximumLength(Room.DescriptionMaximumLength);
+	}
+}
