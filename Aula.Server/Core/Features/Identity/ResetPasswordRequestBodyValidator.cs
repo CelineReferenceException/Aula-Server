e4bbb740fc738e2559f6e6356aca4844b569f661
Rex Aliis <rexaliisdev@gmail.com>
2025-03-24 20:20:27 -0300

    update validator messages

--- Aula.Server/Core/Features/Identity/ResetPasswordRequestBodyValidator.cs
+++ Aula.Server/Core/Features/Identity/ResetPasswordRequestBodyValidator.cs
@@ -1,19 +1,26 @@
-﻿using FluentValidation;
+﻿using Aula.Server.Core.Identity;
+using FluentValidation;
+using Microsoft.Extensions.Options;
 
 namespace Aula.Server.Core.Features.Identity;
 
 internal sealed class ResetPasswordRequestBodyValidator : AbstractValidator<ResetPasswordRequestBody>
 {
-	public ResetPasswordRequestBodyValidator()
+	public ResetPasswordRequestBodyValidator(IOptions<IdentityOptions> options)
 	{
 		_ = RuleFor(x => x.Code)
 			.NotEmpty()
-			.WithErrorCode($"{nameof(ResetPasswordRequestBody.Code)} is empty")
-			.WithMessage($"{nameof(ResetPasswordRequestBody.Code)} cannot be empty.");
+			.WithErrorCode(nameof(ResetPasswordRequestBody.Code))
+			.WithMessage("Cannot be null or empty");
 
 		_ = RuleFor(x => x.NewPassword)
-			.NotEmpty()
-			.WithErrorCode($"{nameof(ResetPasswordRequestBody.NewPassword)} is empty")
-			.WithMessage($"{nameof(ResetPasswordRequestBody.NewPassword)} cannot be empty.");
+			.MinimumLength(options.Value.Password.RequiredLength)
+			.WithErrorCode(nameof(ResetPasswordRequestBody.NewPassword).ToCamel())
+			.WithMessage($"Length must be at least {options.Value.Password.RequiredLength}");
+
+		_ = RuleFor(x => x.NewPassword)
+			.MaximumLength(User.PasswordMaximumLength)
+			.WithErrorCode(nameof(ResetPasswordRequestBody.NewPassword).ToCamel())
+			.WithMessage($"Length must be at most {User.PasswordMaximumLength}");
 	}
 }
