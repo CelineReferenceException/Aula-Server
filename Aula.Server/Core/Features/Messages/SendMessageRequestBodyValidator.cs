272946ba088dde6dde3c42f7ef03752c4ce357cb
Rex Aliis <rexaliisdev@gmail.com>
2025-03-24 20:38:38 -0300

    update validator messages

--- Aula.Server/Core/Features/Messages/SendMessageRequestBodyValidator.cs
+++ Aula.Server/Core/Features/Messages/SendMessageRequestBodyValidator.cs
@@ -6,25 +6,36 @@ internal sealed class SendMessageRequestBodyValidator : AbstractValidator<SendMe
 {
 	public SendMessageRequestBodyValidator()
 	{
-		_ = RuleFor(x => x.Type).IsInEnum();
-		_ = RuleFor(x => x.Flags).IsInEnum();
+		var messageTypes = Enum.GetValues<MessageType>();
+		var messageFlags = Enum.GetValues<MessageFlags>();
+
+		_ = RuleFor(x => x.Type)
+			.IsInEnum()
+			.WithErrorCode(nameof(SendMessageRequestBody.Type).ToCamel())
+			.WithMessage($"Unknown value. Known values are {String.Join(", ", messageTypes.Cast<Int32>())}.");
+
+		_ = RuleFor(x => x.Flags)
+			.IsInEnum()
+			.WithErrorCode(nameof(SendMessageRequestBody.Flags).ToCamel())
+			.WithMessage("Invalid value. " +
+			             $"Valid values are combinations of the following flags: {String.Join(", ", messageFlags.Cast<Int32>())}.");
 
 		_ = When(x => x.Type is MessageType.Standard, () =>
 		{
 			_ = RuleFor(x => x.Content)
 				.NotNull()
-				.WithErrorCode($"{nameof(SendMessageRequestBody.Content)} is null")
-				.WithMessage($"{nameof(SendMessageRequestBody.Content)} cannot be null.");
+				.WithErrorCode(nameof(SendMessageRequestBody.Content).ToCamel())
+				.WithMessage($"Required when {nameof(SendMessageRequestBody.Content).ToCamel()} is {(Int32)MessageType.Standard}.");
 
 			_ = RuleFor(x => x.Content)
 				.MinimumLength(Message.ContentMinimumLength)
-				.WithErrorCode($"{nameof(SendMessageRequestBody.Content)} is too short")
-				.WithMessage($"{nameof(SendMessageRequestBody.Content)} length must be at least {Message.ContentMinimumLength}");
+				.WithErrorCode(nameof(SendMessageRequestBody.Content).ToCamel())
+				.WithMessage($"Length must be at least {Message.ContentMinimumLength}.");
 
 			_ = RuleFor(x => x.Content)
 				.MaximumLength(Message.ContentMaximumLength)
-				.WithErrorCode($"{nameof(SendMessageRequestBody.Content)} is too long")
-				.WithMessage($"{nameof(SendMessageRequestBody.Content)} length must be at most {Message.ContentMaximumLength}");
+				.WithErrorCode(nameof(SendMessageRequestBody.Content).ToCamel())
+				.WithMessage($"Length must be at most {Message.ContentMaximumLength}.");
 		});
 	}
 }
