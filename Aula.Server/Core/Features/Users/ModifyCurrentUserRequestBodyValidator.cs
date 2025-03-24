cabe0d29de42d930823655c7bdeebdce647e10dc
Rex Aliis <rexaliisdev@gmail.com>
2025-03-24 20:55:54 -0300

    update validator messages

--- Aula.Server/Core/Features/Users/ModifyCurrentUserRequestBodyValidator.cs
+++ Aula.Server/Core/Features/Users/ModifyCurrentUserRequestBodyValidator.cs
@@ -8,17 +8,17 @@ internal sealed class ModifyCurrentUserRequestBodyValidator : AbstractValidator<
 	{
 		_ = RuleFor(x => x.DisplayName)
 			.MinimumLength(User.DisplayNameMinimumLength)
-			.WithErrorCode($"{nameof(ModifyCurrentUserRequestBody.DisplayName)} is too short")
-			.WithMessage($"{nameof(ModifyCurrentUserRequestBody.DisplayName)} length must be at least {User.DisplayNameMinimumLength}");
+			.WithErrorCode(nameof(ModifyCurrentUserRequestBody.DisplayName).ToCamel())
+			.WithMessage($"Length must be at least {User.DisplayNameMinimumLength}");
 
 		_ = RuleFor(x => x.DisplayName)
 			.MaximumLength(User.DisplayNameMaximumLength)
-			.WithErrorCode($"{nameof(ModifyCurrentUserRequestBody.DisplayName)} is too long")
-			.WithMessage($"{nameof(ModifyCurrentUserRequestBody.DisplayName)} length must be at most {User.DisplayNameMaximumLength}");
+			.WithErrorCode(nameof(ModifyCurrentUserRequestBody.DisplayName).ToCamel())
+			.WithMessage($"Length must be at most {User.DisplayNameMaximumLength}");
 
 		_ = RuleFor(x => x.Description)
 			.MaximumLength(User.DescriptionMaximumLength)
-			.WithErrorCode($"{nameof(ModifyCurrentUserRequestBody.Description)} is too long")
-			.WithMessage($"{nameof(ModifyCurrentUserRequestBody.Description)} length must be at most {User.DescriptionMaximumLength}");
+			.WithErrorCode(nameof(ModifyCurrentUserRequestBody.Description).ToCamel())
+			.WithMessage($"Length must be at most {User.DescriptionMaximumLength}");
 	}
 }
