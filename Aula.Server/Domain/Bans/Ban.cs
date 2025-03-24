6aee8fc91da71b9062e5888f4f80e15901dc97d1
Rex Aliis <rexaliisdev@gmail.com>
2025-03-24 18:29:17 -0300

    validate new bans with BanValidator

--- Aula.Server/Domain/Bans/Ban.cs
+++ Aula.Server/Domain/Bans/Ban.cs
@@ -1,21 +1,11 @@
-﻿namespace Aula.Server.Domain.Bans;
+﻿using FluentValidation.Results;
+
+namespace Aula.Server.Domain.Bans;
 
 internal sealed class Ban : DefaultDomainEntity
 {
 	internal const Int32 ReasonMinimumLength = 1;
 	internal const Int32 ReasonMaximumLength = 4096;
-
-	private static readonly ResultProblem s_unknownBanType =
-		new("Unknown ban type", "An unknown ban type was provided.");
-
-	private static readonly ResultProblem s_idBanWithNullId =
-		new("Invalid id", "Id bans are required to define a user id.");
-
-	private static readonly ResultProblem s_reasonTooShort =
-		new("Reason is too short", $"Reason length must be at least {ReasonMinimumLength}.");
-
-	private static readonly ResultProblem s_reasonTooLong =
-		new("Reason is too long", $"Reason length must be at most {ReasonMaximumLength}.");
 
 	private Ban(Snowflake id, BanType type, Snowflake? executorId, String? reason, Snowflake? targetId, DateTime creationDate)
 	{
@@ -39,44 +29,21 @@ internal sealed class Ban : DefaultDomainEntity
 
 	internal DateTime CreationDate { get; }
 
-	internal static Result<Ban> Create(
+	internal static Result<Ban, ValidationFailure> Create(
 		Snowflake id,
 		BanType type,
 		Snowflake? executorId = null,
 		String? reason = null,
 		Snowflake? targetId = null)
 	{
-		var problems = new Items<ResultProblem>();
-
-		if (!Enum.IsDefined(type))
-		{
-			problems.Add(s_unknownBanType);
-		}
-
-		if (type is BanType.Id &&
-		    targetId is null)
-		{
-			problems.Add(s_idBanWithNullId);
-		}
-
-		if (reason is not null)
-		{
-			switch (reason.Length)
-			{
-				case < ReasonMinimumLength: problems.Add(s_reasonTooShort); break;
-				case > ReasonMaximumLength: problems.Add(s_reasonTooLong); break;
-				default: break;
-			}
-		}
-
-		if (problems.Count > 0)
-		{
-			return new ResultProblemValues(problems);
-		}
 
 		var ban = new Ban(id, type, executorId, reason, targetId, DateTime.UtcNow);
 		ban.Events.Add(new BanCreatedEvent(ban));
-		return ban;
+
+		var validationResult = BanValidator.Instance.Validate(ban);
+		return validationResult.IsValid
+			? ban
+			: new ResultErrorValues<ValidationFailure>(new Items<ValidationFailure>(validationResult.Errors));
 	}
 
 	internal void Remove()
