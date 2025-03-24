87907027a874cfe9da47d7ccfe235a0726d98f4d
Rex Aliis <rexaliisdev@gmail.com>
2025-03-24 19:00:20 -0300

    validate new messages with MessageValidator

--- Aula.Server/Domain/Messages/Message.cs
+++ Aula.Server/Domain/Messages/Message.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using FluentValidation.Results;
 
 namespace Aula.Server.Domain.Messages;
 
@@ -9,24 +10,6 @@ internal sealed class Message : DefaultDomainEntity
 	internal const MessageFlags UserLeaveTypeAllowedFlags = 0;
 	internal const Int32 ContentMinimumLength = 1;
 	internal const Int32 ContentMaximumLength = 2048;
-
-	private static readonly ResultProblem s_unknownMessageType =
-		new("Unknown message type", "An unknown message type was provided.");
-
-	private static readonly ResultProblem s_unknownMessageFlags =
-		new("Unknown message flags", "One or more of the permissions provided are not valid.");
-
-	private static readonly ResultProblem s_unknownAuthorType =
-		new("Unknown author type", "An unknown author type was provided.");
-
-	private static readonly ResultProblem s_contentTooShort =
-		new("Content is too short", $"Content length must be at least {ContentMinimumLength}.");
-
-	private static readonly ResultProblem s_contentTooLong =
-		new("Content is too long", $"Content length must be at most {ContentMaximumLength}.");
-
-	private static readonly ResultProblem s_userAuthorWithNullId =
-		new("Invalid author", "A user author cannot have a null id.");
 
 	private Message(
 		Snowflake id,
@@ -74,7 +57,7 @@ internal sealed class Message : DefaultDomainEntity
 
 	internal Boolean IsRemoved { get; private set; }
 
-	internal static Result<Message> Create(
+	internal static Result<Message, ValidationFailure> Create(
 		Snowflake id,
 		MessageType type,
 		MessageFlags flags,
@@ -83,23 +66,6 @@ internal sealed class Message : DefaultDomainEntity
 		String? content,
 		Snowflake roomId)
 	{
-		var problems = new Items<ResultProblem>();
-
-		if (!Enum.IsDefined(type))
-		{
-			problems.Add(s_unknownMessageType);
-		}
-
-		if (!Enum.IsDefined(flags))
-		{
-			problems.Add(s_unknownMessageFlags);
-		}
-
-		if (!Enum.IsDefined(authorType))
-		{
-			problems.Add(s_unknownAuthorType);
-		}
-
 		if (flags > 0)
 		{
 			var allowedFlags = type switch
@@ -116,30 +82,13 @@ internal sealed class Message : DefaultDomainEntity
 				.Aggregate((x, y) => x | y);
 		}
 
-		if (authorType is MessageAuthorType.User &&
-		    authorId is null)
-		{
-			problems.Add(s_userAuthorWithNullId);
-		}
-
-		if (content is not null)
-		{
-			switch (content.Length)
-			{
-				case < ContentMinimumLength: problems.Add(s_contentTooShort); break;
-				case > ContentMaximumLength: problems.Add(s_contentTooLong); break;
-				default: break;
-			}
-		}
-
-		if (problems.Count > 0)
-		{
-			return new ResultProblemValues(problems);
-		}
-
 		var message = new Message(id, type, flags, authorType, authorId, roomId, content, DateTime.UtcNow, false);
 		message.Events.Add(new MessageCreatedEvent(message));
-		return message;
+
+		var validationResult = MessageValidator.Instance.Validate(message);
+		return validationResult.IsValid
+			? message
+			: new ResultErrorValues<ValidationFailure>(new Items<ValidationFailure>(validationResult.Errors));
 	}
 
 	internal void Remove()
