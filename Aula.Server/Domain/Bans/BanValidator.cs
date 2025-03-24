aa8bd658618045662b477154ded5fc6ade1d623c
Rex Aliis <rexaliisdev@gmail.com>
2025-03-24 18:17:59 -0300

    add validator messages

--- Aula.Server/Domain/Bans/BanValidator.cs
+++ Aula.Server/Domain/Bans/BanValidator.cs
@@ -6,16 +6,35 @@ internal sealed class BanValidator : AbstractValidator<Ban>
 {
 	public BanValidator()
 	{
-		_ = RuleFor(x => x.Id).NotNull();
+		var banTypes = Enum.GetValues<BanType>();
+
+		_ = RuleFor(x => x.Id)
+			.NotNull()
+			.WithErrorCode(nameof(Ban.Id).ToCamelCase())
+			.WithMessage("Required.");
 
 		_ = RuleFor(x => x.Type)
-			.IsInEnum();
+			.IsInEnum()
+			.WithErrorCode(nameof(Ban.Type).ToCamelCase())
+			.WithMessage($"Unknown type. Known values are {String.Join(", ", banTypes.Select(e => (UInt32)e))}.");
 
 		_ = RuleFor(x => x.Reason)
 			.MinimumLength(Ban.ReasonMinimumLength)
-			.MaximumLength(Ban.ReasonMaximumLength);
+			.WithErrorCode(nameof(Ban.Reason).ToCamelCase())
+			.WithMessage($"Length must be at least {Ban.ReasonMinimumLength}.");
 
-		_ = When(x => x.Type is BanType.Id, () => { _ = RuleFor(x => x.TargetId).NotNull(); });
+		_ = RuleFor(x => x.Reason)
+			.MaximumLength(Ban.ReasonMaximumLength)
+			.WithErrorCode(nameof(Ban.Reason).ToCamelCase())
+			.WithMessage($"Length must be at most {Ban.ReasonMaximumLength}.");
+
+		_ = When(x => x.Type is BanType.Id, () =>
+		{
+			_ = RuleFor(x => x.TargetId)
+				.NotNull()
+				.WithErrorCode(nameof(Ban.TargetId).ToCamelCase())
+				.WithMessage($"Required when {nameof(Type).ToCamelCase()} is {(Int32)BanType.Id}.");
+		});
 	}
 
 	internal static BanValidator Instance { get; } = new();
