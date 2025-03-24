c401d01bdc31cd090928b00baf191ef4e0b5b15e
Rex Aliis <rexaliisdev@gmail.com>
2025-03-24 20:53:16 -0300

    update validator messages

--- Aula.Server/Core/Features/Rooms/CreateRoomRequestBodyValidator.cs
+++ Aula.Server/Core/Features/Rooms/CreateRoomRequestBodyValidator.cs
@@ -8,17 +8,17 @@ internal sealed class CreateRoomRequestBodyValidator : AbstractValidator<CreateR
 	{
 		_ = RuleFor(x => x.Name)
 			.MinimumLength(Room.NameMinimumLength)
-			.WithErrorCode($"{nameof(CreateRoomRequestBody.Name)} is too short")
-			.WithMessage($"{nameof(CreateRoomRequestBody.Name)} length must be at least {Room.NameMinimumLength}");
+			.WithErrorCode(nameof(CreateRoomRequestBody.Name).ToCamel())
+			.WithMessage($"Length must be at least {Room.NameMinimumLength}");
 
 		_ = RuleFor(x => x.Name)
 			.MaximumLength(Room.NameMaximumLength)
-			.WithErrorCode($"{nameof(CreateRoomRequestBody.Name)} is too long")
-			.WithMessage($"{nameof(CreateRoomRequestBody.Name)} length must be at most {Room.NameMaximumLength}");
+			.WithErrorCode(nameof(CreateRoomRequestBody.Name).ToCamel())
+			.WithMessage($"Length must be at most {Room.NameMaximumLength}");
 
 		_ = RuleFor(x => x.Description)
 			.MaximumLength(Room.DescriptionMaximumLength)
-			.WithErrorCode($"{nameof(CreateRoomRequestBody.Description)} is too long")
-			.WithMessage($"{nameof(CreateRoomRequestBody.Description)} length must be at most {Room.DescriptionMaximumLength}");
+			.WithErrorCode(nameof(CreateRoomRequestBody.Description).ToCamel())
+			.WithMessage($"Length must be at most {Room.DescriptionMaximumLength}");
 	}
 }
