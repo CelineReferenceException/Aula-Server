6ccb3cb8bd1c18164c3306a6743aeff89538f284
Rex Aliis <rexaliisdev@gmail.com>
2025-03-24 23:33:32 -0300

    set individual rule for each condition

--- Aula.Server/Core/Domain/Users/UserValidator.cs
+++ Aula.Server/Core/Domain/Users/UserValidator.cs
@@ -6,22 +6,15 @@ internal sealed class UserValidator : AbstractValidator<User>
 {
 	public UserValidator()
 	{
-		_ = RuleFor(x => x.UserName)
-			.NotEmpty()
-			.MinimumLength(User.UserNameMinimumLength)
-			.MaximumLength(User.UserNameMaximumLength);
-
-		_ = RuleFor(x => x.DisplayName)
-			.MinimumLength(User.DisplayNameMinimumLength)
-			.MaximumLength(User.DisplayNameMaximumLength);
-
-		_ = RuleFor(x => x.Description)
-			.MaximumLength(User.DescriptionMaximumLength);
-
+		_ = RuleFor(x => x.UserName).NotEmpty();
+		_ = RuleFor(x => x.UserName).MinimumLength(User.UserNameMinimumLength);
+		_ = RuleFor(x => x.UserName).MaximumLength(User.UserNameMaximumLength);
+		_ = RuleFor(x => x.DisplayName).MinimumLength(User.DisplayNameMinimumLength);
+		_ = RuleFor(x => x.DisplayName).MaximumLength(User.DisplayNameMaximumLength);
+		_ = RuleFor(x => x.Description).MaximumLength(User.DescriptionMaximumLength);
 		_ = RuleFor(x => x.Permissions).IsInEnum();
 		_ = RuleFor(x => x.Type).IsInEnum();
 		_ = RuleFor(x => x.Presence).IsInEnum();
-
 		_ = When(x => x.Type is UserType.Standard, () => { _ = RuleFor(x => x.Email).EmailAddress(); });
 	}
 
