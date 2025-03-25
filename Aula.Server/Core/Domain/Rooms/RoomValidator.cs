9a18fcaa251eda485b01500a47e404578501d5f8
Rex Aliis <rexaliisdev@gmail.com>
2025-03-24 23:38:02 -0300

    remove validator custom messages

--- Aula.Server/Core/Domain/Rooms/RoomValidator.cs
+++ Aula.Server/Core/Domain/Rooms/RoomValidator.cs
@@ -6,30 +6,11 @@ internal sealed class RoomValidator : AbstractValidator<Room>
 {
 	public RoomValidator()
 	{
-		_ = RuleFor(x => x.Name)
-			.NotNull()
-			.WithErrorCode(nameof(Room.Name))
-			.WithMessage("Required");
-
-		_ = RuleFor(x => x.Name)
-			.MinimumLength(Room.NameMinimumLength)
-			.WithErrorCode(nameof(Room.Name))
-			.WithMessage($"Length must be at least {Room.NameMinimumLength}.");
-
-		_ = RuleFor(x => x.Name)
-			.MaximumLength(Room.NameMaximumLength)
-			.WithErrorCode(nameof(Room.Name))
-			.WithMessage($"Length must be at most {Room.NameMaximumLength}.");
-
-		_ = RuleFor(x => x.Description)
-			.NotNull()
-			.WithErrorCode(nameof(Room.Description))
-			.WithMessage("Required");
-
-		_ = RuleFor(x => x.Description)
-			.MaximumLength(Room.DescriptionMaximumLength)
-			.WithErrorCode(nameof(Room.Description))
-			.WithMessage($"Length must be at most {Room.DescriptionMaximumLength}.");
+		_ = RuleFor(x => x.Name).NotNull();
+		_ = RuleFor(x => x.Name).MinimumLength(Room.NameMinimumLength);
+		_ = RuleFor(x => x.Name).MaximumLength(Room.NameMaximumLength);
+		_ = RuleFor(x => x.Description).NotNull();
+		_ = RuleFor(x => x.Description).MaximumLength(Room.DescriptionMaximumLength);
 	}
 
 	internal static RoomValidator Instance { get; } = new();
