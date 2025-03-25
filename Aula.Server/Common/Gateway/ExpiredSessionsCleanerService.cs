49790b25047d20b0c15e795506fa9fb880d8829d
Rex Aliis <rexaliisdev@gmail.com>
2025-03-25 05:57:07 -0300

    define own cleanup interval

--- Aula.Server/Common/Gateway/ExpiredSessionsCleanerService.cs
+++ Aula.Server/Common/Gateway/ExpiredSessionsCleanerService.cs
@@ -3,17 +3,19 @@
 internal sealed class ExpiredSessionsCleanerService : BackgroundService
 {
 	private readonly GatewayService _gatewayService;
+	private readonly TimeSpan _cleanupInterval = TimeSpan.FromMinutes(5);
 
 	public ExpiredSessionsCleanerService(GatewayService gatewayService)
 	{
 		_gatewayService = gatewayService;
+		_cleanupInterval = gatewayService.ExpirePeriod < _cleanupInterval ? gatewayService.ExpirePeriod : _cleanupInterval;
 	}
 
 	protected override async Task ExecuteAsync(CancellationToken stoppingToken)
 	{
 		while (!stoppingToken.IsCancellationRequested)
 		{
-			await Task.Delay(_gatewayService.ExpirePeriod, stoppingToken);
+			await Task.Delay(_cleanupInterval, stoppingToken);
 			_gatewayService.ClearExpiredSessions();
 		}
 	}
