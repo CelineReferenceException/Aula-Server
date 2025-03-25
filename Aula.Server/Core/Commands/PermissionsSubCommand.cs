07a0d1603bfa9b8773a2cec8a131db3a8a43cdc2
Rex Aliis <rexaliisdev@gmail.com>
2025-03-25 07:19:11 -0300

    update command description

--- Aula.Server/Core/Commands/PermissionsSubCommand.cs
+++ Aula.Server/Core/Commands/PermissionsSubCommand.cs
@@ -12,7 +12,7 @@ internal sealed class PermissionsSubCommand : Command
 
 	internal override String Name => "permissions";
 
-	internal override String Description => "User permissions related commands.";
+	internal override String Description => "Shows a list of user permissions related commands.";
 
 	internal override async ValueTask Callback(IReadOnlyDictionary<String, String> args, CancellationToken ct)
 	{
