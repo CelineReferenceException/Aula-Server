e5cb8ad12cd226e846c32f5a9bcf11909b3fbf88
Rex Aliis <rexaliisdev@gmail.com>
2025-03-25 01:12:22 -0300

    inline method

--- Aula.Server/Core/Api/Users/DependencyInjection.cs
+++ Aula.Server/Core/Api/Users/DependencyInjection.cs
@@ -4,14 +4,7 @@ internal static class DependencyInjection
 {
 	internal static IServiceCollection AddUserApi(this IServiceCollection services)
 	{
-		_ = services.AddUserPresenceServices();
-		return services;
-	}
-
-	internal static IServiceCollection AddUserPresenceServices(this IServiceCollection services)
-	{
 		_ = services.AddHostedService<ResetPresencesService>();
-
 		return services;
 	}
 }
