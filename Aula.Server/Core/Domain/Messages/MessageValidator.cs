0fec5a7c60a003e03bd733134c71a74874b22dc9
Rex Aliis <rexaliisdev@gmail.com>
2025-03-24 23:37:11 -0300

    remove validator custom messages

--- Aula.Server/Core/Domain/Messages/MessageValidator.cs
+++ Aula.Server/Core/Domain/Messages/MessageValidator.cs
@@ -6,96 +6,40 @@ internal sealed class MessageValidator : AbstractValidator<Message>
 {
 	public MessageValidator()
 	{
-		var messageTypes = Enum.GetValues<MessageType>();
-		var messageAuthorTypes = Enum.GetValues<MessageAuthorType>();
-
-		_ = RuleFor(x => x.Type)
-			.IsInEnum()
-			.WithErrorCode(nameof(Message.Type))
-			.WithMessage($"Unknown value. Known values are {String.Join(", ", messageTypes.Cast<Int32>())}.");
-
-		_ = RuleFor(x => x.AuthorType)
-			.IsInEnum()
-			.WithErrorCode(nameof(Message.AuthorType))
-			.WithMessage($"Unknown value. Known values are {String.Join(", ", messageAuthorTypes.Cast<Int32>())}.");
+		_ = RuleFor(x => x.Type).IsInEnum();
+		_ = RuleFor(x => x.AuthorType).IsInEnum();
 
 		_ = When(x => x.AuthorType is MessageAuthorType.User, () =>
 		{
-			_ = RuleFor(x => x.AuthorId)
-				.NotNull()
-				.WithErrorCode(nameof(Message.AuthorId))
-				.WithMessage($"Required when {nameof(Message.AuthorType)} is {(Int32)MessageAuthorType.User}.");
+			_ = RuleFor(x => x.AuthorId).NotNull();
 		});
+
 		_ = When(x => x.AuthorType is MessageAuthorType.System, () =>
 		{
-			_ = RuleFor(x => x.AuthorId)
-				.Null()
-				.WithErrorCode(nameof(Message.AuthorId))
-				.WithMessage($"Must be null when {nameof(Message.AuthorType)} is {(Int32)MessageAuthorType.System}.");
+			_ = RuleFor(x => x.AuthorId).Null();
 		});
 
 		_ = When(x => x.Type is MessageType.Standard, () =>
 		{
-			_ = RuleFor(x => x.Content)
-				.NotNull()
-				.WithErrorCode(nameof(Message.Content))
-				.WithMessage($"Required when {nameof(Message.Type)} is {(Int32)MessageType.Standard}.");
-
-			_ = RuleFor(x => x.Content)
-				.MinimumLength(Message.ContentMinimumLength)
-				.WithErrorCode(nameof(Message.Content))
-				.WithMessage($"Length must be at least {Message.ContentMinimumLength}.");
-
-			_ = RuleFor(x => x.Content)
-				.MaximumLength(Message.ContentMaximumLength)
-				.WithErrorCode(nameof(Message.Content))
-				.WithMessage($"Length must be at most {Message.ContentMaximumLength}.");
-
-			_ = RuleFor(x => x.JoinData)
-				.Null()
-				.WithErrorCode(nameof(Message.JoinData))
-				.WithMessage($"Must be null when {nameof(Message.Type)} is {(Int32)MessageType.Standard}.");
-
-			_ = RuleFor(x => x.LeaveData)
-				.Null()
-				.WithErrorCode(nameof(Message.LeaveData))
-				.WithMessage($"Must be null when {nameof(Message.Type)} is {(Int32)MessageType.Standard}.");
+			_ = RuleFor(x => x.Content).NotNull();
+			_ = RuleFor(x => x.Content).MinimumLength(Message.ContentMinimumLength);
+			_ = RuleFor(x => x.Content).MaximumLength(Message.ContentMaximumLength);
+			_ = RuleFor(x => x.JoinData).Null();
+			_ = RuleFor(x => x.LeaveData).Null();
 		});
 
 		_ = When(x => x.Type is MessageType.UserJoin, () =>
 		{
-			_ = RuleFor(x => x.Content)
-				.Null()
-				.WithErrorCode(nameof(Message.Content))
-				.WithMessage($"Must be null when {nameof(Message.Type)} is {(Int32)MessageType.UserJoin}.");
-
-			_ = RuleFor(x => x.JoinData)
-				.NotNull()
-				.WithErrorCode(nameof(Message.JoinData))
-				.WithMessage($"Required when {nameof(Message.Type)} is {(Int32)MessageType.UserJoin}.");
-
-			_ = RuleFor(x => x.LeaveData)
-				.Null()
-				.WithErrorCode(nameof(Message.LeaveData))
-				.WithMessage($"Must be null when {nameof(Message.Type)} is {(Int32)MessageType.UserJoin}.");
+			_ = RuleFor(x => x.Content).Null();
+			_ = RuleFor(x => x.JoinData).NotNull();
+			_ = RuleFor(x => x.LeaveData).Null();
 		});
 
 		_ = When(x => x.Type is MessageType.UserLeave, () =>
 		{
-			_ = RuleFor(x => x.Content)
-				.Null()
-				.WithErrorCode(nameof(Message.Content))
-				.WithMessage($"Must be null when {nameof(Message.Type)} is {(Int32)MessageType.UserLeave}.");
-
-			_ = RuleFor(x => x.JoinData)
-				.Null()
-				.WithErrorCode(nameof(Message.JoinData))
-				.WithMessage($"Must be null when {nameof(Message.Type)} is {(Int32)MessageType.UserLeave}.");
-
-			_ = RuleFor(x => x.LeaveData)
-				.NotNull()
-				.WithErrorCode(nameof(Message.LeaveData))
-				.WithMessage($"Required when {nameof(Message.Type)} is {(Int32)MessageType.UserLeave}.");
+			_ = RuleFor(x => x.Content).Null();
+			_ = RuleFor(x => x.JoinData).Null();
+			_ = RuleFor(x => x.LeaveData).NotNull();
 		});
 	}
 
